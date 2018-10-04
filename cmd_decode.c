3276ed785e527dbf1efe59fd1da9480a02b91a57
David Given <dg@cowlark.com>
2018-10-04 22:36:36 +0200

    Replace the MFM decoder logic with something which very nearly works. Now I search for the sync region before each record and recalculate the clock from that. Unfortunately, knowing when I've reached the end of a record and so should start resyncing is hard without parsing the records themselves, and so I frequently lose byte phase between records.

--- cmd_decode.c
+++ cmd_decode.c
@@ -3,17 +3,30 @@
 #include <unistd.h>
 #include <string.h>
 
+#define CLOCK_DETECTOR_AMPLITUDE_THRESHOLD 100 /* arbitrary */
+#define CLOCK_ERROR_BOUNDS 0.25
+
 static const char* inputfilename = NULL;
 static const char* outputfilename = NULL;
 static sqlite3* indb;
 static sqlite3* outdb;
+
+static const uint8_t* inputbuffer;
+static int inputlen;
+static int cursor;
+
+static int period0; /* lower limit for a short transition */
+static int period1; /* between short and medium transitions */
+static int period2; /* between medium and long transitions */
+static int period3; /* upper limit for a long transition */
 
 static uint32_t buckets[128];
 static uint8_t outputbuffer[100*1024];
 static int outputbufferpos;
 static uint8_t fifo = 0;
 static int bitcount = 0;
-static int unsynced = false;
+static bool phaselocked = false;
+static bool phase = false;
 
 static void syntax_error(void)
 {
@@ -58,81 +71,100 @@ static void open_files(void)
     atexit(close_files);
 }
 
-static int find_peak(int* cursor)
+static void find_clock(void)
 {
-    while (*cursor < 128)
+    /* Somewhere in the bitstream there'll be a sync sequence of 16 0 bytes
+     * followed by a special index byte, beginning with a 1 bit.
+     * 
+     * These zeroes will be encoded as 10 10 10 10..., so forming a nice
+     * simple signal that should be easy to detect. This routine scans the
+     * bitstream until it finds one of these windows, and sets the clock
+     * accordingly. Remember that the routine can be easily spoofed by bad
+     * data, so you need to check for the marker byte afterwards.
+     * 
+     * ...btw, the standard fill byte is 0x4e:
+     * 
+     *     0  1  0  0  1  1  1  0
+     *    10 01 00 10 01 01 01 00
+     * 
+     * That's four medium transitions vs two short ones. So we know we're going
+     * to miscalculate the clock the first time round.
+     */
+
+    uint32_t buckets[128] = {};
+    for (;;)
     {
-        if (buckets[*cursor] > 100)
+        if (cursor >= inputlen)
+            return;
+        uint8_t data = inputbuffer[cursor++];
+        if (data & 0x80)
+            continue;
+        buckets[data] += 4;
+
+        /* The bucket list slowly decays. */
+
+        for (int i=0; i<128; i++)
+            if (buckets[i] > 0)
+                buckets[i]--;
+
+        /* 
+         * After 10 bytes, we'll have seen 80 bits. So there should be a nice
+         * sharp peak in our distribution. The amplitude is chosen by trial and
+         * error. *
+         */
+        
+        uint32_t maxvalue = 0;
+        int maxindex = 0;
+        for (int i=0; i<128; i++)
         {
-            /* This is high enough that it's probably the start of a spike.
-             * Find the end, tracking the highest value. */
-
-            int maxindex = *cursor;
-            int maxvalue = 0;
-            while (*cursor < 128)
+            if (buckets[i] > maxvalue)
             {
-                int v = buckets[*cursor];
-                if (v < 10)
-                    break;
-                if (v > maxvalue)
-                {
-                    maxvalue = v;
-                    maxindex = *cursor;
-                }
-
-                (*cursor)++;
+                maxvalue = buckets[i];
+                maxindex = i;
             }
-            return maxindex;
         }
 
-        (*cursor)++;
+        if (maxvalue > CLOCK_DETECTOR_AMPLITUDE_THRESHOLD)
+        {
+            /* 
+             * Okay, this looks good. We'll assume this is clock/2 --- 250kHz
+             * for HD floppies; this is one short transition. We're also going
+             * to assume that this was a 0 bit and set the phase, god help us.
+             */
+
+            double short_time = maxindex;
+            double medium_time = short_time * 1.5;
+            double long_time = short_time * 2.0;
+
+            period0 = short_time - short_time * CLOCK_ERROR_BOUNDS;
+            period1 = (short_time + medium_time) / 2.0;
+            period2 = (medium_time + long_time) / 2.0;
+            period3 = long_time + long_time * CLOCK_ERROR_BOUNDS;
+            phase = true;
+            phaselocked = true;
+            return;
+        }
     }
-    return -1;
 }
 
 static void queue_bit(bool bit)
 {
     fifo <<= 1;
     fifo |= bit;
-
-    if (unsynced && (fifo == 0x4e))
-    {
-        bitcount = 7;
-        unsynced = false;
-    }
-
-    if (bitcount == 7)
-    {
-        outputbuffer[outputbufferpos++] = fifo;
-        bitcount = 0;
-    }
-    else
-        bitcount++;
+    bitcount++;
 }
 
-static void decode_track_cb(int track, int side, const uint8_t* data, size_t len)
+static bool read_bit(void)
 {
-    printf("Track %d side %d: ", track, side);
+    static bool queued = false;
+    static bool has_queued = false;
+    if (has_queued)
+    {
+        has_queued = false;
+        return queued;
+    }
 
-    memset(buckets, 0, sizeof(buckets));
-    for (int i=0; i<len; i++)
-        buckets[data[i] & 0x7f]++;
-
-    int cursor = 0;
-    int peak1 = find_peak(&cursor);
-    int peak2 = find_peak(&cursor);
-    int peak3 = find_peak(&cursor);
-    int peak4 = find_peak(&cursor);
-    if ((peak3 == -1) || (peak4 != -1))
-        error("don't understand disk (something's wrong with the encoding)");
-
-    double freq = (double)TICK_FREQUENCY / (double)(peak3 - peak1);
-    int split1 = (peak1 + peak2) / 2;
-    int split2 = (peak2 + peak3) / 2;
-    int split3 = peak3 + (split2 - split1);
-    printf("freq %d kHz\n", (int)(freq/1000.0));
-
-    /* 
+    /*
      * MFM divides the signal into two-bit cells, which can be either x0
      * (representing 0) and 01 (representing 1). x can be any value; the rules
      * set it to 0 of the previous cell contained 01, and 1 otherwise.
@@ -158,86 +190,98 @@ static void decode_track_cb(int track, int side, const uint8_t* data, size_t len
      * Data:      0  0  1  1  0  0  1  0  0  0  0  0 |  1  0  1  0  1  0
      * Signal: 0 10 10 01 01 00 10 01 00 10 10 10 00 | x1 00 01 00 01 00
      *                                               \ resync
-     * 
-     * But there's more. Even after we get the bitstream, we don't know
-     * where byte boundaries are. So, certain values are written to the
-     * disk with missing bits:
-     * 
-     * Data: C2    1  1  0  0  0  0  1  0
-     * Should be: 01 01 00 10 10 10 01 10
-     * Actually:  01 01 00 10 00 10 01 10
-     *                        ^ wrong
-     * 
-     * What'll happen is that we'll resync in the wrong place and all our data
-     * is gibberish. So we need to explicitly check the fifo for sync bytes and
-     * cope.
      */
 
-    outputbufferpos = 0;
-    unsynced = true;
-    bool phase = false;
-    for (int i=0; i<len; i++)
+    if (cursor >= inputlen)
+        return false;
+    uint8_t t = inputbuffer[cursor++];
+    if (t & 0x80)
+        t = 0x7f;
+
+    if ((t < period0) || (t > period3))
     {
-        int t = data[i];
-        if ((t & 0x80) || (t > split3))
-        {
-            unsynced = true;
-            continue;
-        }
+        /* Garbage data: our clock's wrong. */
+        phaselocked = false;
+        return false;
+    }
+    else if (t < period1)
+    {
+        /* Short transition: either (1),01, or ,(1)0,1. */
+        return !phase;
+    }
+    else if (t > period2)
+    {
+        /* Long transition: either (1),00,01 or ,(1)0,00,1.
+         * The latter is illegal but occurs inside marker bytes. */
 
-        if (t > split2)
+        if (!phase)
         {
-            /* Three zeroes.
-             *  1,00,01
-             *    ^^ ^^
-             */
-            queue_bit(false);
-            if ((fifo & 0x0f) == 0x0c)
-            {
-                /* This is a sync byte. */
-                phase = true;
-            }
-            else
-            {
-                /* Not a sync byte --- resync. */
-
-                phase = false;
-            }
-            queue_bit(!phase);
-        }
-        else if (t < split1)
-        {
-            /* One zero: either 1,01, or ,10,1. */
-            queue_bit(!phase);
+            queued = true;
+            has_queued = true;
+            return false;
         }
         else
         {
-            /* Two zeroes. */
-            if (!phase)
-            {
-                /*
-                 * 1,00,1
-                 *   ^^
-                 */
-                queue_bit(false);
-                phase = true;
-            }
-            else
-            {
-                /* 
-                 * ,10,01,
-                 *   ^ ^
-                 */
-                queue_bit(false);
-                queue_bit(true);
-                phase = false;
-            }
+            queued = false;
+            has_queued = true;
+            return false;
         }
     }
+    else
+    {
+        /* Medium transition: either (1),00,1 or ,(1)0,01. */
+        if (!phase)
+        {
+            phase = true;
+            return false;
+        }
+        else
+        {
+            queued = true;
+            has_queued = true;
+            phase = false;
+            return false;
+        }
+    }
+}
 
-    /* Flush. */
-    for (int i=0; i<8; i++)
-        queue_bit(false);
+static uint8_t read_byte(void)
+{
+    while (phaselocked && (bitcount < 8))
+        queue_bit(read_bit());
+    bitcount = 0;
+    return fifo;
+}
+
+static void decode_track_cb(int track, int side, const uint8_t* data, size_t len)
+{
+    printf("Track %d side %d: ", track, side);
+
+    inputbuffer = data;
+    inputlen = len;
+    cursor = 0;
+
+    while (cursor < inputlen)
+    {
+        if (!phaselocked)
+        {
+            printf("lost phase sync at byte %x\n", outputbufferpos);
+            while (cursor < inputlen)
+            {
+                find_clock();
+                while (phaselocked)
+                {
+                    if (read_bit())
+                        goto found_byte;
+                }
+            }
+        found_byte:;
+            fifo = 1;
+            bitcount = 1;
+        }
+
+        outputbuffer[outputbufferpos++] = read_byte();
+    }
 
     FILE* fp = fopen("test.dat", "wb");
     fwrite(outputbuffer, 1, outputbufferpos, fp);
