d38952df2e38e1607d92f131ae335cc98f95857f
David Given <dg@cowlark.com>
2019-01-02 17:17:40 +0100

    Measured the Brother sector spacing. Thinking about gaps.

--- src/fe-writebrother.cc
+++ src/fe-writebrother.cc
@@ -15,25 +15,25 @@ static StringFlag inputFilename(
     "The input image file to read from.",
     "brother.img");
 
-static IntFlag clockRate(
+static DoubleFlag clockRateUs(
 	{ "--clock-rate" },
-	"Encoded data clock rate (nanoseconds).",
-	3850);
+	"Encoded data clock rate (microseconds).",
+	3.850);
 
-static IntFlag postIndexGap(
+static IntFlag postIndexGapMs(
 	{ "--post-index-gap" },
-	"Post-index gap before first sector header (nanoseconds).",
-	100000);
+	"Post-index gap before first sector header (milliseconds).",
+	2.0);
 
-static IntFlag sectorSpacing(
+static DoubleFlag sectorSpacingMs(
 	{ "--sector-spacing" },
-	"Time between successive sector heads (nanoseconds).",
-	100000);
+	"Time between successive sector heads (milliseconds).",
+	16.684);
 
 static StringFlag sectorSkew(
 	{ "--sector-skew" },
 	"Order in which to write sectors.",
-	"0123456789ab");
+	"05a3816b4927");
 
 int main(int argc, const char* argv[])
 {
@@ -43,22 +43,26 @@ int main(int argc, const char* argv[])
 	Geometry geometry = {78, 1, 12, 256};
 	readSectorsFromFile(allSectors, geometry, inputFilename);
 
-	int lastSectorAt = postIndexGap + geometry.sectors*sectorSpacing;
-	if (lastSectorAt > 200000000)
-		Error() << "with that spacing, some sectors won't fit in a track";
+	double lastSectorAtMs = postIndexGapMs + geometry.sectors*sectorSpacingMs;
+	if (lastSectorAtMs > 200.0)
+		Error() << "with that spacing, some sectors won't fit in a track "
+				<< fmt::format("({:.3f}ms out of 200.0ms)", lastSectorAtMs);
 
-	int bitsPerRevolution = 200000000 / clockRate;
+	int bitsPerRevolution = 200000.0 / clockRateUs;
 	std::cerr << bitsPerRevolution << " bits per 200ms revolution" << std::endl
-	          << "post-index gap: " << postIndexGap << "ns" << std::endl
-			  << "pre-index gap: " << (200000000 - lastSectorAt) << "ns" << std::endl;
+	          << fmt::format("post-index gap: {:.3f}ms\n", postIndexGapMs)
+			  << fmt::format("pre-index gap: {:.3f}ms\n", 200.0 - lastSectorAtMs);
 
+	const std::string& skew = sectorSkew;
 	for (int track=0; track<geometry.tracks; track++)
 	{
 		std::vector<bool> bits(bitsPerRevolution);
 
 		for (int sectorCount=0; sectorCount<geometry.sectors; sectorCount++)
 		{
-			int cursor = postIndexGap + sectorCount*sectorSpacing;
+			int sectorId = skew.at(sectorCount) - '0';
+			double cursorMs = postIndexGapMs + sectorCount*sectorSpacingMs;
+			std::cerr << "logical sector " << sectorId << " at pos " << cursorMs << std::endl;
 		}
 
 	}
