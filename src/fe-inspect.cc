dbceb88e4f0c982467eab1dd9065419c581c3ff5
David Given <dg@cowlark.com>
2019-01-23 01:20:26 +0100

    Simplified the fe-inspect UI.

--- src/fe-inspect.cc
+++ src/fe-inspect.cc
@@ -7,16 +7,6 @@
 #include "protocol.h"
 #include <fmt/format.h>
 
-static IntFlag trackFlag(
-    { "--track", "-T" },
-    "Which track to inspect.",
-    0);
-
-static IntFlag sideFlag(
-    { "--side", "-S" },
-    "Which side to inspect.",
-    0);
-
 static DoubleFlag clockScaleFlag(
 	{ "--clock-scale" },
 	"Scale the clock by this much after detection (use 0.5 for MFM, 1.0 for anything else).",
@@ -39,74 +29,73 @@ int main(int argc, const char* argv[])
 {
     Flag::parseFlags(argc, argv);
 
-    for (auto& track : readTracks())
-    {
-		if ((track->track == trackFlag) && (track->side == sideFlag))
+	const auto& tracks = readTracks();
+	if (tracks.size() != 1)
+		Error() << "the source dataspec must contain exactly one track (two sides count as two tracks)";
+
+	auto& track = *tracks.begin();
+	std::unique_ptr<Fluxmap> fluxmap = track->read();
+
+	nanoseconds_t clockPeriod = fluxmap->guessClock();
+	std::cout << fmt::format("       {:.2f} us clock detected; ", (double)clockPeriod/1000.0) << std::flush;
+
+	clockPeriod *= clockScaleFlag;
+	std::cout << fmt::format("{:.2f} us bit clock; ", (double)clockPeriod/1000.0) << std::flush;
+
+	auto bitmap = fluxmap->decodeToBits(clockPeriod);
+	std::cout << fmt::format("{} bytes encoded.", bitmap.size()/8) << std::endl;
+
+	if (dumpFluxFlag)
+	{
+		std::cout << std::endl
+					<< "Magnetic flux follows (times in us):" << std::endl;
+
+		int resolution = fluxmapResolutionFlag;
+		if (resolution == 0)
+			resolution = clockPeriod / 4;
+
+		nanoseconds_t now = 0;
+		nanoseconds_t nextclock = clockPeriod;
+		int ticks = 0;
+		std::cout << fmt::format("{: 10.3f}:-", 0.0);
+		for (int cursor=0; cursor<fluxmap->bytes(); cursor++)
 		{
-			std::unique_ptr<Fluxmap> fluxmap = track->read();
+			int interval = (*fluxmap)[cursor];
+			if (interval == 0)
+				interval = 0x100;
+			ticks += interval;
 
-			nanoseconds_t clockPeriod = fluxmap->guessClock();
-			std::cout << fmt::format("       {:.2f} us clock detected; ", (double)clockPeriod/1000.0) << std::flush;
-
-			clockPeriod *= clockScaleFlag;
-			std::cout << fmt::format("{:.2f} us bit clock; ", (double)clockPeriod/1000.0) << std::flush;
-
-			auto bitmap = fluxmap->decodeToBits(clockPeriod);
-			std::cout << fmt::format("{} bytes encoded.", bitmap.size()/8) << std::endl;
-
-			if (dumpFluxFlag)
+			nanoseconds_t transition = ticks*NS_PER_TICK;
+			nanoseconds_t next;
+			
+			bool clocked = false;
+			for (;;)
 			{
+				next = now + resolution;
+				clocked = now >= nextclock;
+				if (clocked)
+					nextclock += clockPeriod;
+				if (next >= transition)
+					break;
 				std::cout << std::endl
-				          << "Magnetic flux follows (times in us):" << std::endl;
-
-				int resolution = fluxmapResolutionFlag;
-				if (resolution == 0)
-					resolution = clockPeriod / 4;
-
-				nanoseconds_t now = 0;
-				nanoseconds_t nextclock = clockPeriod;
-				int ticks = 0;
-				std::cout << fmt::format("{: 10.3f}:-", 0.0);
-				for (int cursor=0; cursor<fluxmap->bytes(); cursor++)
-				{
-					int interval = (*fluxmap)[cursor];
-					if (interval == 0)
-						interval = 0x100;
-					ticks += interval;
-
-					nanoseconds_t transition = ticks*NS_PER_TICK;
-					nanoseconds_t next;
-					
-					bool clocked = false;
-					for (;;)
-					{
-						next = now + resolution;
-						clocked = now >= nextclock;
-						if (clocked)
-							nextclock += clockPeriod;
-						if (next >= transition)
-							break;
-						std::cout << std::endl
-						          << fmt::format("{: 10.3f}:{}", (double)next / 1000.0, clocked ? '-' : ' ');
-						now = next;
-					}
-
-					std::cout << fmt::format("==== {: 10.3f}", (double)transition / 1000.0);
-				}
+							<< fmt::format("{: 10.3f}:{}", (double)next / 1000.0, clocked ? '-' : ' ');
+				now = next;
 			}
 
-			if (dumpBitstreamFlag)
-			{
-				std::cout << "Aligned bitstream of length " << bitmap.size()
-                          << " follows:" << std::endl
-						  << std::endl;
-
-				for (bool bit : bitmap)
-					std::cout << (bit ? 'X' : '-');
-				std::cout << std::endl;
-			}
+			std::cout << fmt::format("==== {: 10.3f}", (double)transition / 1000.0);
 		}
-    }
+	}
+
+	if (dumpBitstreamFlag)
+	{
+		std::cout << "Aligned bitstream of length " << bitmap.size()
+					<< " follows:" << std::endl
+					<< std::endl;
+
+		for (bool bit : bitmap)
+			std::cout << (bit ? 'X' : '-');
+		std::cout << std::endl;
+	}
 
     return 0;
 }
