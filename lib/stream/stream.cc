bf6509bff53514e88596a02536eeadb754b9b4d7
David Given <dg@cowlark.com>
2019-02-21 00:49:15 +0100

    Tiny cleanup.

--- lib/stream/stream.cc
+++ lib/stream/stream.cc
@@ -73,12 +73,13 @@ std::unique_ptr<Fluxmap> readStream(const std::string& path, unsigned track, uns
                 else if (b == 0x09)
                 {
                     /* Nop2: skip one byte */
-                    f.seekg(1, std::ios_base::cur);
+                    f.get();
                 }
                 else if (b == 0x0a)
                 {
                     /* Nop3: skip two bytes */
-                    f.seekg(2, std::ios_base::cur);
+                    f.get();
+                    f.get();
                 }
                 else if (b == 0x0b)
                 {   /* Ovl16: the next block is 0x10000 sclks longer than normal.
