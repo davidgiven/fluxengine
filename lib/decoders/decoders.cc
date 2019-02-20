e53841f79daf3ba59ebbd0f5ca0e574a269c8862
David Given <dg@cowlark.com>
2019-02-20 22:09:06 +0100

    Use a bit more statistical nous when trying to sense the bit rate, and use the median of the pulse to pick the normative clock rate.

--- lib/decoders/decoders.cc
+++ lib/decoders/decoders.cc
@@ -5,11 +5,6 @@
 #include "protocol.h"
 #include "fmt/format.h"
 
-static IntFlag clockDetectionNoiseFloor(
-    { "--clock-detection-noise-floor" },
-    "Noise floor used for clock detection in flux.",
-    200);
-
 static DoubleFlag clockDecodeThreshold(
     { "--clock-decode-threshold" },
     "Pulses below this fraction of a clock tick are considered spurious and ignored.",
@@ -29,45 +24,78 @@ nanoseconds_t Fluxmap::guessClock() const
     for (uint8_t interval : _intervals)
         buckets[interval]++;
     
-    int peaklo = 1;
-    while (peaklo < 256)
+    uint32_t max = *std::max_element(std::begin(buckets), std::end(buckets));
+    uint32_t min = *std::min_element(std::begin(buckets), std::end(buckets));
+    uint32_t noise_floor = (min+max)/100;
+    uint32_t signal_level = noise_floor * 5;
+
+    /* Find a point solidly within the first pulse. */
+
+    int pulseindex = 0;
+    while (pulseindex < 256)
     {
-        if (buckets[peaklo] > (uint32_t)(clockDetectionNoiseFloor*2))
+        if (buckets[pulseindex] > signal_level)
             break;
-        peaklo++;
+        pulseindex++;
+    }
+    if (pulseindex == -1)
+        return 0;
+
+    /* Find the upper and lower bounds of the pulse. */
+
+    int peaklo = pulseindex;
+    while (peaklo > 0)
+    {
+        if (buckets[peaklo] < noise_floor)
+            break;
+        peaklo--;
     }
 
-    uint32_t peakmaxindex = peaklo;
-    uint32_t peakmaxvalue = buckets[peakmaxindex];
-    uint32_t peakhi = peaklo;
-    while (peakhi < 256)
+    int peakhi = pulseindex;
+    while (peakhi < 255)
     {
-        uint32_t v = buckets[peakhi];
-        if (buckets[peakhi] < (uint32_t)clockDetectionNoiseFloor)
+        if (buckets[peakhi] < noise_floor)
             break;
-        if (v > peakmaxvalue)
-        {
-            peakmaxindex = peakhi;
-            peakmaxvalue = v;
-        }
         peakhi++;
+    }
+
+    /* Find the total accumulated size of the pulse. */
+
+    uint32_t total_size = 0;
+    for (int i = peaklo; i < peakhi; i++)
+        total_size += buckets[i];
+
+    /* Now find the median. */
+
+    uint32_t count = 0;
+    int median = peaklo;
+    while (median < peakhi)
+    {
+        count += buckets[median];
+        if (count > (total_size/2))
+            break;
+        median++;
     }
 
     if (showClockHistogram)
     {
         std::cout << "Clock detection histogram:" << std::endl;
         for (int i=0; i<256; i++)
-        {
-            std::cout << fmt::format("{:.2f} {}\n", (double)i * US_PER_TICK, buckets[i]);
-        }
+            std::cout << fmt::format("{:.2f} {}", (double)i * US_PER_TICK, buckets[i]) << std::endl;
+
+        std::cout << fmt::format("Noise floor:  {}", noise_floor) << std::endl;
+        std::cout << fmt::format("Signal level: {}", signal_level) << std::endl;
+        std::cout << fmt::format("Peak start:   {} ({:.2f} us)", peaklo, peaklo*US_PER_TICK) << std::endl;
+        std::cout << fmt::format("Peak end:     {} ({:.2f} us)", peakhi, peakhi*US_PER_TICK) << std::endl;
+        std::cout << fmt::format("Median:       {} ({:.2f} us)", median, median*US_PER_TICK) << std::endl;
     }
 
     /* 
-     * Okay, peakmaxindex should now be a good candidate for the (or a) clock.
+     * Okay, the median should now be a good candidate for the (or a) clock.
      * How this maps onto the actual clock rate depends on the encoding.
      */
 
-    return peakmaxindex * NS_PER_TICK;
+    return median * NS_PER_TICK;
 }
 
 /* Decodes a fluxmap into a nice aligned array of bits. */
