6d9a73bd52650368953fdd6f31a8f9482865dce7
David Given <dg@cowlark.com>
2019-02-25 00:47:27 +0100

    GCR decoding works (AFAICT).

--- lib/c64/decoder.cc
+++ lib/c64/decoder.cc
@@ -10,6 +10,39 @@
 #include <string.h>
 #include <algorithm>
 
+static int decode_data_gcr(uint8_t gcr)
+{
+    switch (gcr)
+    {
+		#define GCR_ENTRY(gcr, data) \
+			case gcr: return data;
+		#include "data_gcr.h"
+		#undef GCR_ENTRY
+    }
+    return -1;
+};
+
+static std::vector<uint8_t> decode(const std::vector<bool>& bits)
+{
+    BitAccumulator ba;
+
+    auto ii = bits.begin();
+    while (ii != bits.end())
+    {
+        uint8_t inputfifo = 0;
+        for (size_t i=0; i<5; i++)
+        {
+            if (ii == bits.end())
+                break;
+            inputfifo = (inputfifo<<1) | *ii++;
+        }
+
+        ba.push(decode_data_gcr(inputfifo), 4);
+    }
+
+    return ba;
+}
+
 SectorVector Commodore64Decoder::decodeToSectors(const RawRecordVector& rawRecords, unsigned)
 {
     std::vector<std::unique_ptr<Sector>> sectors;
@@ -17,6 +50,10 @@ SectorVector Commodore64Decoder::decodeToSectors(const RawRecordVector& rawRecor
     for (auto& rawrecord : rawRecords)
     {
         const auto& rawdata = rawrecord->data;
+        const auto& rawbytes = decode(rawdata);
+
+        if (rawbytes.size() == 0)
+            continue;
 	}
 
 	return sectors;
@@ -31,6 +68,6 @@ int Commodore64Decoder::recordMatcher(uint64_t fifo) const
 {
     uint32_t masked = fifo & 0xffffff;
     if ((masked == C64_SECTOR_RECORD) || (masked == C64_DATA_RECORD))
-		return 24;
+		return 12;
     return 0;
 }
