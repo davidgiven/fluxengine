60bfe050d3e4ed88366bffc5cf49ae21ccdaed89
David Given <dg@cowlark.com>
2019-08-21 00:45:10 +0200

    Refactor the way image extensions are handled to be generally cleaner. Add support for validating ImageSpecs before we actually want to read/write an image, so as to allow us to check the extension *before* wasting time reading a disk. Make .d81 an alias of .img.

--- lib/imagereader/imagereader.cc
+++ lib/imagereader/imagereader.cc
@@ -7,6 +7,13 @@
 #include "imagereader/imagereader.h"
 #include "fmt/format.h"
 
+std::map<std::string, ImageReader::Constructor> ImageReader::formats =
+{
+	{".adf", ImageReader::createImgImageReader},
+	{".d81", ImageReader::createImgImageReader},
+	{".img", ImageReader::createImgImageReader},
+};
+
 static bool ends_with(const std::string& value, const std::string& ending)
 {
     if (ending.size() > value.size())
@@ -14,15 +21,29 @@ static bool ends_with(const std::string& value, const std::string& ending)
     return std::equal(ending.rbegin(), ending.rend(), value.rbegin());
 }
 
-std::unique_ptr<ImageReader> ImageReader::create(const ImageSpec& spec)
+ImageReader::Constructor ImageReader::findConstructor(const ImageSpec& spec)
 {
     const auto& filename = spec.filename;
 
-    if (ends_with(filename, ".img") || ends_with(filename, ".adf"))
-        return createImgImageReader(spec);
+	for (const auto& e : formats)
+	{
+		if (ends_with(filename, e.first))
+			return e.second;
+	}
 
-    Error() << "unrecognised image filename extension";
-    return std::unique_ptr<ImageReader>();
+	return NULL;
+}
+
+std::unique_ptr<ImageReader> ImageReader::create(const ImageSpec& spec)
+{
+    verifyImageSpec(spec);
+    return findConstructor(spec)(spec);
+}
+
+void ImageReader::verifyImageSpec(const ImageSpec& spec)
+{
+    if (!findConstructor(spec))
+        Error() << "unrecognised image filename extension";
 }
 
 ImageReader::ImageReader(const ImageSpec& spec):
