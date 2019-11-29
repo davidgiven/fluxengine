3094c5c919e70e630b714e51f504f43ab95bdba1
David Given <dg@cowlark.com>
2019-11-29 18:49:22 +0100

    Add missing files.

---
+++ FluxEngine.cydsn/FIFOout/API/c.c
@@ -0,0 +1,29 @@
+#include "cyfitter_cfg.h"
+#include "cydevice_trm.h"
+#include "cyfitter.h"
+#include "`$INSTANCE_NAME`_h.h"
+
+void `$INSTANCE_NAME`_Start()
+{
+   `$INSTANCE_NAME`_Init();
+}    
+
+void `$INSTANCE_NAME`_Stop()
+{
+    `$INSTANCE_NAME`_Disable();
+}
+
+void `$INSTANCE_NAME`_Init()
+{    
+    `$INSTANCE_NAME`_Enable();
+    
+}
+void `$INSTANCE_NAME`_Enable()
+{
+}
+
+void `$INSTANCE_NAME`_Disable()
+{
+}
+
+/* [] END OF FILE */
