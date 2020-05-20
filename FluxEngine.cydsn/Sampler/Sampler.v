adb9809692f022d9b5ad866b2d1ffe0bd8403148
Jared Boone <jboone@earfeast.com>
2020-05-20 11:34:29 -0700

    Simplify Sampler.

--- FluxEngine.cydsn/Sampler/Sampler.v
+++ FluxEngine.cydsn/Sampler/Sampler.v
@@ -1,7 +1,6 @@
 
 //`#start header` -- edit after this line, do not edit this line
 `include "cypress.v"
-`include "../SuperCounter/SuperCounter.v"
 
 //`#end` -- edit above this line, do not edit this line
 // Generated on 12/11/2019 at 21:18
@@ -9,7 +8,7 @@
 module Sampler (
 	output [2:0] debug_state,
 	output reg [7:0] opcode,
-	output  req,
+	output reg       req,
 	input   clock,
 	input   index,
 	input   rdata,
@@ -19,77 +18,60 @@ module Sampler (
 
 //`#start body` -- edit after this line, do not edit this line
 
-localparam STATE_WAITING = 0;
-localparam STATE_OPCODE = 1;
-
-reg [0:0] state;
 reg [5:0] counter;
 
-reg oldsampleclock;
-reg oldindex;
-reg oldrdata;
+reg sampleclock_q;
+reg index_q;
+reg rdata_q;
 
-reg sampleclocked;
-reg indexed;
-reg rdataed;
-
-assign req = (state == STATE_OPCODE);
+reg sampleclock_edge;
+reg index_edge;
+reg rdata_edge;
 
 always @(posedge clock)
 begin
     if (reset)
     begin
-        state <= STATE_WAITING;
-        opcode <= 0;
-        sampleclocked <= 0;
-        indexed <= 0;
-        rdataed <= 0;
-        oldsampleclock <= 0;
-        oldindex <= 0;
-        oldrdata <= 0;
+        sampleclock_edge <= 0;
+        index_edge <= 0;
+        rdata_edge <= 0;
+        sampleclock_q <= 0;
+        index_q <= 0;
+        rdata_q <= 0;
         counter <= 0;
     end
     else
     begin
         /* Remember positive egdes for sampleclock, index and rdata. */
         
-        if (sampleclock && !oldsampleclock)
-            sampleclocked <= 1;
-        oldsampleclock <= sampleclock;
+        /* Positive-going edge detection of 16 MHz square-wave sample clock vs.
+         * 64 MHz clock.
+         */
+        sampleclock_edge <= sampleclock && !sampleclock_q;
+        sampleclock_q <= sampleclock;
         
-        if (index && !oldindex)
-            indexed <= 1;
-        oldindex <= index;
+        /* Request to write FIFO is inactive by default */
+        req <= 0;
         
-        if (rdata && !oldrdata)
-            rdataed <= 1;
-        oldrdata <= rdata;
-        
-        case (state)
-            STATE_WAITING:
-            begin
-                if (sampleclocked)
-                begin
-                    if (rdataed || indexed || (counter == 6'h3f))
-                    begin
-                        opcode <= {rdataed, indexed, counter};
-                        rdataed <= 0;
-                        indexed <= 0;
-                        counter <= 1; /* remember to count this tick */
-                        state <= STATE_OPCODE;
-                    end
-                    else
-                        counter <= counter + 1;
-                        
-                    sampleclocked <= 0;
-                end
-            end
+        if (sampleclock_edge) begin
+            /* Both index and rdata are active high -- positive-going edges
+             * indicate the start of an index pulse and read pulse, respectively.
+             */
+             
+            index_edge <= index && !index_q;
+            index_q <= index;
             
-            STATE_OPCODE: /* opcode sent here */
-            begin
-                state <= STATE_WAITING;
+            rdata_edge <= rdata && !rdata_q;
+            rdata_q <= rdata;
+            
+            if (rdata_edge || index_edge || (counter == 6'h3f)) begin
+                opcode <= { rdata_edge, index_edge, counter };
+                req <= 1;
+                counter <= 1;
+            end else begin
+                counter <= counter + 1;
             end
-        endcase
+        end
     end
 end
 
