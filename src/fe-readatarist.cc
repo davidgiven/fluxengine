46e987e393730b056fd97c2a0427550340e0289c
David Given <dg@cowlark.com>
2021-02-16 23:07:25 +0100

    Refactor the IBM readers so they just set flags and readibm.cc is doing all the work.

--- src/fe-readatarist.cc
+++ src/fe-readatarist.cc
@@ -1,24 +1,14 @@
 #include "globals.h"
-#include "flags.h"
 #include "reader.h"
-#include "fluxmap.h"
-#include "decoders/decoders.h"
-#include "sector.h"
-#include "sectorset.h"
-#include "record.h"
-#include "dataspec.h"
 #include "ibm/ibm.h"
 #include "fmt/format.h"
-
-static FlagGroup flags { &readerFlags };
+#include "readibm.h"
 
 int mainReadAtariST(int argc, const char* argv[])
 {
 	setReaderDefaultSource(":t=0-79:s=0-1");
 	setReaderDefaultOutput("atarist.st");
-    flags.parseFlags(argc, argv);
-
-	IbmDecoder decoder(1);
-	readDiskCommand(decoder);
-    return 0;
+	sectorIdBase.setDefaultValue(1);
+	return mainReadIBM(argc, argv);
 }
+
---
+++ src/readibm.h
@@ -0,0 +1,14 @@
+#ifndef READIBM_H
+#define READIBM_H
+
+#include "flags.h"
+#include "dataspec.h"
+
+extern IntFlag sectorIdBase;
+extern BoolFlag ignoreSideByte;
+extern RangeFlag requiredSectors;
+
+extern int mainReadIBM(int argc, const char* argv[]);
+
+#endif
+
