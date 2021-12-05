da7e2b1b542228eb7d677c97eeb663a4989ff56f
David Given <dg@cowlark.com>
2021-12-05 20:41:20 +0000

    Add a menu.

--- src/gui/main.cc
+++ src/gui/main.cc
@@ -3,12 +3,30 @@
 
 static uiWindow* mainwin;
 
+static int close_cb(uiWindow* window, void* data)
+{
+	uiQuit();
+	return 1;
+}
+
+static int quit_cb(void* data)
+{
+	return 1;
+}
+
 int main(int argc, const char* argv[])
 {
 	uiInitOptions o = {0};
 	uiInit(&o);
 
+	uiMenu* menu = uiNewMenu("File");
+	uiMenuItem* item = uiMenuAppendQuitItem(menu);
+	uiOnShouldQuit(quit_cb, NULL);
+
 	mainwin = uiNewWindow("example", 640, 480, 1);
+	uiWindowOnClosing(mainwin, close_cb, NULL);
+	uiOnShouldQuit(quit_cb, NULL);
+
 	uiControlShow(uiControl(mainwin));
 	uiMain();
 	return 0;
