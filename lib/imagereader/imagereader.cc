23b9e9ef5f7f1c6052f13061e16c965128ef4b84
David Given <dg@cowlark.com>
2022-02-27 00:20:20 +0100

    The image readers now log to the logger.

--- lib/imagereader/imagereader.cc
+++ lib/imagereader/imagereader.cc
@@ -12,83 +12,149 @@
 
 std::unique_ptr<ImageReader> ImageReader::create(const ImageReaderProto& config)
 {
-	switch (config.format_case())
-	{
-		case ImageReaderProto::kDim:
-			return ImageReader::createDimImageReader(config);
+    switch (config.format_case())
+    {
+        case ImageReaderProto::kDim:
+            return ImageReader::createDimImageReader(config);
 
-		case ImageReaderProto::kD88:
-			return ImageReader::createD88ImageReader(config);
+        case ImageReaderProto::kD88:
+            return ImageReader::createD88ImageReader(config);
 
-		case ImageReaderProto::kFdi:
-			return ImageReader::createFdiImageReader(config);
+        case ImageReaderProto::kFdi:
+            return ImageReader::createFdiImageReader(config);
 
-		case ImageReaderProto::kImd:
-			return ImageReader::createIMDImageReader(config);
+        case ImageReaderProto::kImd:
+            return ImageReader::createIMDImageReader(config);
 
-		case ImageReaderProto::kImg:
-			return ImageReader::createImgImageReader(config);
+        case ImageReaderProto::kImg:
+            return ImageReader::createImgImageReader(config);
 
-		case ImageReaderProto::kDiskcopy:
-			return ImageReader::createDiskCopyImageReader(config);
+        case ImageReaderProto::kDiskcopy:
+            return ImageReader::createDiskCopyImageReader(config);
 
-		case ImageReaderProto::kJv3:
-			return ImageReader::createJv3ImageReader(config);
+        case ImageReaderProto::kJv3:
+            return ImageReader::createJv3ImageReader(config);
 
-		case ImageReaderProto::kD64:
-			return ImageReader::createD64ImageReader(config);
+        case ImageReaderProto::kD64:
+            return ImageReader::createD64ImageReader(config);
 
-		case ImageReaderProto::kNfd:
-			return ImageReader::createNFDImageReader(config);
+        case ImageReaderProto::kNfd:
+            return ImageReader::createNFDImageReader(config);
 
-		case ImageReaderProto::kNsi:
-			return ImageReader::createNsiImageReader(config);
+        case ImageReaderProto::kNsi:
+            return ImageReader::createNsiImageReader(config);
 
-		case ImageReaderProto::kTd0:
-			return ImageReader::createTd0ImageReader(config);
+        case ImageReaderProto::kTd0:
+            return ImageReader::createTd0ImageReader(config);
 
-		default:
-			Error() << "bad input file config";
-			return std::unique_ptr<ImageReader>();
-	}
+        default:
+            Error() << "bad input file config";
+            return std::unique_ptr<ImageReader>();
+    }
 }
 
-void ImageReader::updateConfigForFilename(ImageReaderProto* proto, const std::string& filename)
+void ImageReader::updateConfigForFilename(
+    ImageReaderProto* proto, const std::string& filename)
 {
-	static const std::map<std::string, std::function<void(void)>> formats =
-	{
-		{".adf",      [&]() { proto->mutable_img(); }},
-		{".d64",      [&]() { proto->mutable_d64(); }},
-		{".d81",      [&]() { proto->mutable_img(); }},
-		{".d88",      [&]() { proto->mutable_d88(); }},
-		{".dim",      [&]() { proto->mutable_dim(); }},
-		{".diskcopy", [&]() { proto->mutable_diskcopy(); }},
-		{".dsk",      [&]() { proto->mutable_img(); }},
-		{".fdi",      [&]() { proto->mutable_fdi(); }},
-		{".imd",      [&]() { proto->mutable_imd(); }},
-		{".img",      [&]() { proto->mutable_img(); }},
-		{".jv3",      [&]() { proto->mutable_jv3(); }},
-		{".nfd",      [&]() { proto->mutable_nfd(); }},
-		{".nsi",      [&]() { proto->mutable_nsi(); }},
-		{".st",       [&]() { proto->mutable_img(); }},
-		{".td0",      [&]() { proto->mutable_td0(); }},
-		{".vgi",      [&]() { proto->mutable_img(); }},
-		{".xdf",      [&]() { proto->mutable_img(); }},
-	};
+    static const std::map<std::string, std::function<void(void)>> formats = {
+        {".adf",
+         [&]()
+            {
+                proto->mutable_img();
+            }},
+        {".d64",
+         [&]()
+            {
+                proto->mutable_d64();
+            }},
+        {".d81",
+         [&]()
+            {
+                proto->mutable_img();
+            }},
+        {".d88",
+         [&]()
+            {
+                proto->mutable_d88();
+            }},
+        {".dim",
+         [&]()
+            {
+                proto->mutable_dim();
+            }},
+        {".diskcopy",
+         [&]()
+            {
+                proto->mutable_diskcopy();
+            }},
+        {".dsk",
+         [&]()
+            {
+                proto->mutable_img();
+            }},
+        {".fdi",
+         [&]()
+            {
+                proto->mutable_fdi();
+            }},
+        {".imd",
+         [&]()
+            {
+                proto->mutable_imd();
+            }},
+        {".img",
+         [&]()
+            {
+                proto->mutable_img();
+            }},
+        {".jv3",
+         [&]()
+            {
+                proto->mutable_jv3();
+            }},
+        {".nfd",
+         [&]()
+            {
+                proto->mutable_nfd();
+            }},
+        {".nsi",
+         [&]()
+            {
+                proto->mutable_nsi();
+            }},
+        {".st",
+         [&]()
+            {
+                proto->mutable_img();
+            }},
+        {".td0",
+         [&]()
+            {
+                proto->mutable_td0();
+            }},
+        {".vgi",
+         [&]()
+            {
+                proto->mutable_img();
+            }},
+        {".xdf",
+         [&]()
+            {
+                proto->mutable_img();
+            }},
+    };
 
-	for (const auto& it : formats)
-	{
-		if (endsWith(filename, it.first))
-		{
-			it.second();
-			proto->set_filename(filename);
-			return;
-		}
-	}
+    for (const auto& it : formats)
+    {
+        if (endsWith(filename, it.first))
+        {
+            it.second();
+            proto->set_filename(filename);
+            return;
+        }
+    }
 
-	Error() << fmt::format("unrecognised image filename '{}'", filename);
+    Error() << fmt::format("unrecognised image filename '{}'", filename);
 }
 
-ImageReader::ImageReader(const ImageReaderProto& config):
-    _config(config)
-{}
+ImageReader::ImageReader(const ImageReaderProto& config): _config(config) {}
--- lib/imagereader/imgimagereader.cc
+++ lib/imagereader/imgimagereader.cc
@@ -3,6 +3,7 @@
 #include "sector.h"
 #include "imagereader/imagereader.h"
 #include "image.h"
+#include "logger.h"
 #include "lib/config.pb.h"
 #include "imginputoutpututils.h"
 #include "fmt/format.h"
@@ -13,85 +14,91 @@
 class ImgImageReader : public ImageReader
 {
 public:
-	ImgImageReader(const ImageReaderProto& config):
-		ImageReader(config)
-	{}
+    ImgImageReader(const ImageReaderProto& config): ImageReader(config) {}
 
-	std::unique_ptr<Image> readImage()
-	{
-        std::ifstream inputFile(_config.filename(), std::ios::in | std::ios::binary);
+    std::unique_ptr<Image> readImage()
+    {
+        std::ifstream inputFile(
+            _config.filename(), std::ios::in | std::ios::binary);
         if (!inputFile.is_open())
             Error() << "cannot open input file";
 
-		if (!_config.img().tracks() || !_config.img().sides())
-			Error() << "IMG: bad configuration; did you remember to set the tracks, sides and trackdata fields?";
+        if (!_config.img().tracks() || !_config.img().sides())
+            Error() << "IMG: bad configuration; did you remember to set the "
+                       "tracks, sides and trackdata fields?";
 
         std::unique_ptr<Image> image(new Image);
-		for (const auto& p : getTrackOrdering(_config.img()))
-		{
-			int track = p.first;
-			int side = p.second;
+        for (const auto& p : getTrackOrdering(_config.img()))
+        {
+            int track = p.first;
+            int side = p.second;
 
-			if (inputFile.eof())
-				break;
-			int physicalCylinder = track * _config.img().physical_step() + _config.img().physical_offset();
+            if (inputFile.eof())
+                break;
+            int physicalCylinder = track * _config.img().physical_step() +
+                                   _config.img().physical_offset();
 
-			ImgInputOutputProto::TrackdataProto trackdata;
-			getTrackFormat(_config.img(), trackdata, track, side);
+            ImgInputOutputProto::TrackdataProto trackdata;
+            getTrackFormat(_config.img(), trackdata, track, side);
 
-			for (int sectorId : getSectors(trackdata))
-			{
-				Bytes data(trackdata.sector_size());
-				inputFile.read((char*) data.begin(), data.size());
+            for (int sectorId : getSectors(trackdata))
+            {
+                Bytes data(trackdata.sector_size());
+                inputFile.read((char*)data.begin(), data.size());
 
-				const auto& sector = image->put(physicalCylinder, side, sectorId);
-				sector->status = Sector::OK;
-				sector->logicalTrack = track;
-				sector->physicalCylinder = physicalCylinder;
-				sector->logicalSide = sector->physicalHead = side;
-				sector->logicalSector = sectorId;
-				sector->data = data;
-			}
+                const auto& sector =
+                    image->put(physicalCylinder, side, sectorId);
+                sector->status = Sector::OK;
+                sector->logicalTrack = track;
+                sector->physicalCylinder = physicalCylinder;
+                sector->logicalSide = sector->physicalHead = side;
+                sector->logicalSector = sectorId;
+                sector->data = data;
+            }
         }
 
-		image->calculateSize();
-		const Geometry& geometry = image->getGeometry();
-        std::cout << fmt::format("IMG: read {} tracks, {} sides, {} kB total\n",
-                        geometry.numTracks, geometry.numSides,
-						inputFile.tellg() / 1024);
+        image->calculateSize();
+        const Geometry& geometry = image->getGeometry();
+        Logger() << fmt::format("IMG: read {} tracks, {} sides, {} kB total",
+            geometry.numTracks,
+            geometry.numSides,
+            inputFile.tellg() / 1024);
         return image;
-	}
+    }
 
-	std::vector<unsigned> getSectors(const ImgInputOutputProto::TrackdataProto& trackdata)
-	{
-		std::vector<unsigned> sectors;
-		switch (trackdata.sectors_oneof_case())
-		{
-			case ImgInputOutputProto::TrackdataProto::SectorsOneofCase::kSectors:
-			{
-				for (int sectorId : trackdata.sectors().sector())
-					sectors.push_back(sectorId);
-				break;
-			}
+    std::vector<unsigned> getSectors(
+        const ImgInputOutputProto::TrackdataProto& trackdata)
+    {
+        std::vector<unsigned> sectors;
+        switch (trackdata.sectors_oneof_case())
+        {
+            case ImgInputOutputProto::TrackdataProto::SectorsOneofCase::
+                kSectors:
+            {
+                for (int sectorId : trackdata.sectors().sector())
+                    sectors.push_back(sectorId);
+                break;
+            }
 
-			case ImgInputOutputProto::TrackdataProto::SectorsOneofCase::kSectorRange:
-			{
-				int sectorId = trackdata.sector_range().start_sector();
-				for (int i=0; i<trackdata.sector_range().sector_count(); i++)
-					sectors.push_back(sectorId + i);
-				break;
-			}
+            case ImgInputOutputProto::TrackdataProto::SectorsOneofCase::
+                kSectorRange:
+            {
+                int sectorId = trackdata.sector_range().start_sector();
+                for (int i = 0; i < trackdata.sector_range().sector_count();
+                     i++)
+                    sectors.push_back(sectorId + i);
+                break;
+            }
 
-			default:
-				Error() << "no list of sectors provided in track format";
-		}
-		return sectors;
-	}
+            default:
+                Error() << "no list of sectors provided in track format";
+        }
+        return sectors;
+    }
 };
 
 std::unique_ptr<ImageReader> ImageReader::createImgImageReader(
-	const ImageReaderProto& config)
+    const ImageReaderProto& config)
 {
     return std::unique_ptr<ImageReader>(new ImgImageReader(config));
 }
-
