9842c9945d2aef9f8983c36dab0387f08958df2d
David Given <dg@cowlark.com>
2022-03-05 00:33:10 +0100

    Typo fix.

--- lib/utils.cc
+++ lib/utils.cc
@@ -8,7 +8,7 @@ static const char* SEPARATORS = "/\\";
 
 void ErrorException::print() const
 {
-	std::cerr << message << '\n';
+    std::cerr << message << '\n';
 }
 
 bool beginsWith(const std::string& value, const std::string& ending)
@@ -23,47 +23,52 @@ bool endsWith(const std::string& value, const std::string& ending)
 {
     if (ending.size() > value.size())
         return false;
+
     std::string lowercase(ending.size(), 0);
-    std::transform(value.rbegin(), value.rbegin() + ending.size(), lowercase.begin(), [](unsigned char c){ return std::tolower(c); });
+    std::transform(value.rbegin(),
+        value.rbegin() + ending.size(),
+        lowercase.begin(),
+        [](unsigned char c)
+        {
+            return std::tolower(c);
+        });
+
     return std::equal(ending.rbegin(), ending.rend(), value.rbegin()) ||
-        std::equal(ending.rbegin(), ending.rend(), lowercase.begin());
+           std::equal(ending.rbegin(), ending.rend(), lowercase.begin());
 }
 
 std::string leftTrimWhitespace(std::string value)
 {
-	value.erase(0, value.find_first_not_of(WHITESPACE));
-	return value;
+    value.erase(0, value.find_first_not_of(WHITESPACE));
+    return value;
 }
 
 std::string rightTrimWhitespace(std::string value)
 {
-	value.erase(value.find_last_not_of(WHITESPACE) + 1);
-	return value;
+    value.erase(value.find_last_not_of(WHITESPACE) + 1);
+    return value;
 }
 
 std::string trimWhitespace(const std::string& value)
 {
-	return leftTrimWhitespace(rightTrimWhitespace(value));
+    return leftTrimWhitespace(rightTrimWhitespace(value));
 }
 
 std::string getLeafname(const std::string& value)
 {
-	constexpr char sep = '/';
-	#ifdef _WIN32
-		sep = '\\';
-	#endif
+    constexpr char sep = '/';
 
-   size_t i = value.find_last_of(SEPARATORS);
-   if (i != std::string::npos) {
-      return value.substr(i+1, value.length() - i);
-   }
+    size_t i = value.find_last_of(SEPARATORS);
+    if (i != std::string::npos)
+    {
+        return value.substr(i + 1, value.length() - i);
+    }
 
-   return value;
+    return value;
 }
 
 void testForEmergencyStop()
 {
-	if (emergencyStop)
-		throw EmergencyStopException();
+    if (emergencyStop)
+        throw EmergencyStopException();
 }
-
