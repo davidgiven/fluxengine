52407848c1695f050cf8e0805621bafb5c1164be
David Given <dg@cowlark.com>
2022-03-19 13:55:22 +0100

    Do more sanity checks for bad sectors, improving the reads by a lot.

--- arch/agat/decoder.cc
+++ arch/agat/decoder.cc
@@ -16,14 +16,16 @@
  * data:    X  X  X  X   X  X  X  X   -  X  X  -   X  -  X  -   X  -  -  X   -  X  -  X  = 0xff6a95
  * flux:   01 01 01 01  01 01 01 01  00 01 01 00  01 00 01 00  01 00 10 01  00 01 00 01  = 0x555514444911
  * 
- * (We just ignore this one --- it's useless and optional.)
+ * Each pattern is prefixed with this one:
+ *
+ * flux:   10 00  10 01  00 10  01 00 = 8924
  */
 
-static const uint64_t SECTOR_ID = 0x555549111444;
-static const FluxPattern SECTOR_PATTERN(48, SECTOR_ID);
+static const uint64_t SECTOR_ID = 0x8924555549111444;
+static const FluxPattern SECTOR_PATTERN(64, SECTOR_ID);
 
-static const uint64_t DATA_ID = 0x555514444911;
-static const FluxPattern DATA_PATTERN(48, DATA_ID);
+static const uint64_t DATA_ID = 0x8924555514444911;
+static const FluxPattern DATA_PATTERN(64, DATA_ID);
 
 static const FluxMatchers ALL_PATTERNS = {
 	&SECTOR_PATTERN,
@@ -44,7 +46,7 @@ public:
 
     void decodeSectorRecord() override
 	{
-		if (readRaw48() != SECTOR_ID)
+		if (readRaw64() != SECTOR_ID)
 			return;
 
 		auto bytes = decodeFmMfm(readRawBits(64)).slice(0, 4);
@@ -59,10 +61,13 @@ public:
 
 	void decodeDataRecord() override
 	{
-		if (readRaw48() != DATA_ID)
+		if (readRaw64() != DATA_ID)
 			return;
 
 		Bytes bytes = decodeFmMfm(readRawBits((AGAT_SECTOR_SIZE+2)*16)).slice(0, AGAT_SECTOR_SIZE+2);
+
+		if (bytes[AGAT_SECTOR_SIZE+1] != 0x5a)
+			return;
 
 		_sector->data = bytes.slice(0, AGAT_SECTOR_SIZE);
 		uint8_t wantChecksum = bytes[AGAT_SECTOR_SIZE];
