5e8f35c94e4ed17b30f613d5e0c76760ac292dbb
Jeff Epler <jepler@gmail.com>
2022-03-29 10:54:15 -0500

    Fix review items, try again on applesauce compatibility
    After these changes, applesauce accepts the image but produces a
    non-working result.  passport.py still likes the files just fine, if
    they're of the limited sorts that it can handle.
    
    (applesauce wants just one STRM chunk with all the flux from all the
    tracks; the whole STRM ends with an extra FF, which is not counted in the
    size of any of the TIMING blocks. passport.py / a2archery is considerably
    more forgiving)

--- lib/fluxsink/a2rfluxsink.cc
+++ lib/fluxsink/a2rfluxsink.cc
@@ -6,8 +6,10 @@
 #include "fluxsink/fluxsink.h"
 #include "decoders/fluxmapreader.h"
 #include "lib/fluxsink/fluxsink.pb.h"
+#include "lib/logger.h"
 #include "proto.h"
 #include "fmt/format.h"
+#include "fmt/chrono.h"
 #include "fluxmap.h"
 #include "a2r.h"
 #include <fstream>
@@ -33,24 +35,24 @@ public:
 		_writer{_bytes.writer()}
 	{
 
-		std::cout << fmt::format("A2R: writing A2R {} file containing {} tracks\n",
+		Logger() << fmt::format("A2R: writing A2R {} file containing {} tracks\n",
 			singlesided() ? "single sided" : "double sided",
 			config.cylinders().end() - config.cylinders().start() + 1
 		);
 
-		char image_date[100];
 		time_t now{std::time(nullptr)};
-		strftime(image_date, sizeof(image_date), "%FT%TZ", gmtime(&now));
-		_metadata["image_date"] = image_date;
-
-		writeHeader();
-		writeInfo();
-		writeMeta();
+		auto t = gmtime(&now);
+		_metadata["image_date"] = fmt::format("{:%FT%TZ}", *t);
 	}
 
 	~A2RFluxSink()
 	{
-		std::cout << "A2R: writing output file...\n";
+		writeHeader();
+		writeInfo();
+		writeStream();
+		writeMeta();
+
+		Logger() << "A2R: writing output file...\n";
 		std::ofstream of(_config.filename(), std::ios::out | std::ios::binary);
 		if (!of.is_open())
 			Error() << "cannot open output file";
@@ -80,9 +82,10 @@ private:
 		    1,
 		    1,
 		};
-		char buf[33];
-		snprintf(buf, sizeof(buf), "Fluxengine %d%32.32s", FLUXENGINE_VERSION, "");
-		memcpy(info.creator, buf, sizeof(info.creator));
+		auto version_str = fmt::format("Fluxengine {}", FLUXENGINE_VERSION);
+		auto version_str_padded = fmt::format("{: <32}", version_str);
+		assert(version_str_padded.size() == sizeof(info.creator));
+		memcpy(info.creator, version_str_padded.data(), sizeof(info.creator));
 		writeChunkAndData(A2R_CHUNK_INFO, &info, sizeof(info));
 	}
 
@@ -94,75 +97,102 @@ private:
 		writeChunkAndData(A2R_CHUNK_META, ss.str().data(), ss.str().size());
 	}
 
+	void writeStream() {
+		// A STRM always ends with a 255, even though this could ALSO indicate the first byte of a multi-byte sequence
+		_strmWriter.write_8(255);
+
+		writeChunkAndData(A2R_CHUNK_STRM, _strmBytes.cbegin(), _strmBytes.size());
+	}
+
 	void writeFlux(int cylinder, int head, const Fluxmap& fluxmap) override
 	{
 		// Note: this assumes that 'cylinder' numbers are in the PC high density 5.25 drive numbering, NOT apple numbering
 		unsigned location = cylinder * 2 + head;
-		    std::cout << fmt::format("location {} cylinder {} head {}\n", location, cylinder, head);
+
+		if(!fluxmap.bytes()) {
+		    return;
+		}
 
 		if (location > 255) {
-		    std::cout << fmt::format("A2R: cannot write track {} head {}, "
+		    Error() << fmt::format("A2R: cannot write track {} head {}, "
 			    "{} does not fit within the location field\n",
 			    cylinder, head, location);
 		    return;
 		}
 
-		FluxmapReader fmr(fluxmap);
 
-		// A timing stream always starts at an index. It continues for nominally 450 degress of rotation, 250ms at 300RPM
-		fmr.skipToEvent(F_BIT_INDEX);
-		// but maybe there is no index, if we're writing from an image to an a2r
-		if(fmr.eof()) fmr.rewind();
-
-		int revolution = 0;
-		uint32_t totalTicks = 0;
-		uint32_t loopPoint = 0;
-		unsigned ticksSinceLastPulse = 0;
+		// Writing from an image (as opposed to from a floppy) will contain exactly one revolution and no index events.
+		auto is_image = [](auto &fluxmap) {
+			FluxmapReader fmr(fluxmap);
+			fmr.skipToEvent(F_BIT_INDEX);
+			// but maybe there is no index, if we're writing from an image to an a2r
+			return fmr.eof();
+		};
 
 		// Write the flux data into its own Bytes
 		Bytes trackBytes;
 		auto trackWriter = trackBytes.writer();
-		while(!fmr.eof()) {
-			unsigned ticks;
-			int event;
-			fmr.getNextEvent(event, ticks);
 
-			ticksSinceLastPulse += ticks;
-			totalTicks += ticks;
+		auto write_one_flux = [&](unsigned ticks) {
+			auto value = ticks_to_a2r(ticks);
+			while(value > 254) {
+				trackWriter.write_8(255);
+				value -= 255;
+			}
+			trackWriter.write_8(value);
+		};
 
-			if(event & F_BIT_PULSE) {
-				auto value = ticks_to_a2r(ticksSinceLastPulse);
-				while(value > 254) {
-					trackWriter.write_8(255);
-					value -= 255;
+		int revolution = 0;
+		uint32_t loopPoint = 0;
+		uint32_t totalTicks = 0;
+		FluxmapReader fmr(fluxmap);
+
+		auto write_flux = [&](unsigned maxTicks = ~0u) {
+			unsigned ticksSinceLastPulse = 0;
+
+			while(!fmr.eof() && totalTicks < maxTicks) {
+				unsigned ticks;
+				int event;
+				fmr.getNextEvent(event, ticks);
+
+				ticksSinceLastPulse += ticks;
+				totalTicks += ticks;
+
+				if(event & F_BIT_PULSE) {
+					write_one_flux(ticksSinceLastPulse);
+					ticksSinceLastPulse = 0;
 				}
-				trackWriter.write_8(value);
-				ticksSinceLastPulse = 0;
+
+				if(event & F_BIT_INDEX && revolution == 0) {
+					loopPoint = totalTicks;
+					revolution += 1;
+				}
 			}
 
-			if(event & F_BIT_INDEX && revolution == 0) {
-				loopPoint = ticks_to_a2r(totalTicks);
-				revolution += 1;
-			}
+		};
+
+		if(is_image(fluxmap)) {
+			// A timing stream with no index represents exactly one
+			// revolution with no index. However, a2r nominally contains 450
+			// degress of rotation, 250ms at 300rpm.
+			write_flux();
+			loopPoint = totalTicks;
+			fmr.rewind();
+			revolution += 1;
+			write_flux(totalTicks*5/4);
+		} else {
+			// We have an index, so this is real from a floppy and should be "one revolution plus a bit"
+			fmr.skipToEvent(F_BIT_INDEX);
+			write_flux();
 		}
 
-		// A STRM always ends with a 255, even though this could ALSO indicate the first byte of a multi-byte sequence
-		trackWriter.write_8(255);
-
-		// if writing an from an image to a2r, there's also no actual loop point, fake it
-		if(loopPoint == 0) { 
-			loopPoint = ticks_to_a2r(totalTicks);
-		}
-
-		std::cerr << fmt::format("loopPoint={} track size {}\n", loopPoint, trackBytes.size());
 		uint32_t chunk_size = 10 + trackBytes.size();
-		_writer.write_le32(A2R_CHUNK_STRM);
-		_writer.write_le32(chunk_size);
-		_writer.write_8(location);
-		_writer.write_8(A2R_TIMING);
-		_writer.write_le32(trackBytes.size());
-		_writer.write_le32(loopPoint);
-		_writer += trackBytes;
+
+		_strmWriter.write_8(location);
+		_strmWriter.write_8(A2R_TIMING);
+		_strmWriter.write_le32(trackBytes.size());
+		_strmWriter.write_le32(ticks_to_a2r(loopPoint));
+		_strmWriter += trackBytes;
 	}
 
 	operator std::string () const
@@ -175,6 +205,8 @@ private:
 	A2RHeader _fileheader = {0};
 	Bytes _bytes;
 	ByteWriter _writer;
+	Bytes _strmBytes;
+	ByteWriter _strmWriter{_strmBytes.writer()};
 	std::map<std::string, std::string> _metadata;
 };
 }
