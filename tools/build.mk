62258be400dfbbc8a660f7b89cdd75df16e33220
David Given <dg@cowlark.com>
2022-06-14 01:07:20 +0200

    Add missing file.

---
+++ tools/build.mk
@@ -0,0 +1,17 @@
+brother120tool.exe: $(OBJDIR)/tools/brother120tool.o
+brother240tool.exe: $(OBJDIR)/tools/brother240tool.o
+upgrade-flux-file.exe: $(OBJDIR)/tools/upgrade-flux-file.o
+
+$(call use-library, brother120tool.exe, $(OBJDIR)/tools/brother120tool.o, PROTO)
+$(call use-library, brother120tool.exe, $(OBJDIR)/tools/brother120tool.o, LIBFLUXENGINE)
+$(call use-library, brother120tool.exe, $(OBJDIR)/tools/brother120tool.o, PROTO)
+
+$(call use-library, brother240tool.exe, $(OBJDIR)/tools/brother240tool.o, PROTO)
+$(call use-library, brother240tool.exe, $(OBJDIR)/tools/brother240tool.o, LIBFLUXENGINE)
+
+$(call use-pkgconfig, upgrade-flux-file.exe, $(OBJDIR)/tools/upgrade-flux-file.o, sqlite3)
+$(call use-library, upgrade-flux-file.exe, $(OBJDIR)/tools/upgrade-flux-file.o, LIBFLUXENGINE)
+$(call use-library, upgrade-flux-file.exe, $(OBJDIR)/tools/upgrade-flux-file.o, PROTO)
+
+all: brother120tool.exe brother240tool.exe upgrade-flux-file.exe
+
