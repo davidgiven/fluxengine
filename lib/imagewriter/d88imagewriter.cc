davidgiven/fluxengine

commit 4ba30fcfec39f42fdc76fc3828e2075c3a924c43

Don't require C++20 as it's still too new.

lib/imagewriter/d88imagewriter.cc
@@ -11,6 +11,17 @@
 #include <iostream>
 #include <fstream>
 
+static int countl_zero(uint32_t value)
+{
+	int count = 0;
+	while (!(value & 0x8000000))
+	{
+		value <<= 1;
+		count++;
+	}
+	return count;
+}
+
 class D88ImageWriter : public ImageWriter
 {
 public:
@@ -72,7 +83,7 @@ public:
 				sectorWriter.write_8(sector->logicalTrack);
 				sectorWriter.write_8(sector->logicalSide);
 				sectorWriter.write_8(sector->logicalSector);
-				sectorWriter.write_8(24 - std::countl_zero(uint32_t(sector->data.size())));
+				sectorWriter.write_8(24 - countl_zero(uint32_t(sector->data.size())));
 				sectorWriter.write_le16(sectors.size());
 				sectorWriter.write_8(0x00); // always write mfm
 				sectorWriter.write_8(0x00); // always write not deleted data

commit 416c70f5e568b236c25465d75f9cd8b86709b017

Fix D88 writer regression caused by 4ba30fcf.

lib/imagewriter/d88imagewriter.cc
@@ -14,7 +14,7 @@
 static int countl_zero(uint32_t value)
 {
 	int count = 0;
-	while (!(value & 0x8000000))
+	while (!(value & 0x80000000))
 	{
 		value <<= 1;
 		count++;