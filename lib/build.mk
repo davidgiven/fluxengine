5625cf5254e7c09d3dd271037f017eb1a6eb4e3e
dtrg <dtrg>
2022-08-29 13:41:53 +0000

    More hfs boilerplate.

--- lib/build.mk
+++ lib/build.mk
@@ -64,12 +64,13 @@ LIBFLUXENGINE_SRCS = \
 	lib/usb/usb.cc \
 	lib/usb/usbfinder.cc \
 	lib/utils.cc \
-	lib/vfs/brother120fs.cc \
 	lib/vfs/acorndfs.cc \
 	lib/vfs/amigaffs.cc \
+	lib/vfs/brother120fs.cc \
 	lib/vfs/cpmfs.cc \
-	lib/vfs/vfs.cc \
 	lib/vfs/fatfs.cc \
+	lib/vfs/machfs.cc \
+	lib/vfs/vfs.cc \
 	lib/vfs/fluxsectorinterface.cc \
 	lib/vfs/imagesectorinterface.cc \
 
@@ -87,5 +88,6 @@ $(LIBFLUXENGINE_OBJS): CFLAGS += $(LIBUSBP_CFLAGS)
 $(LIBFLUXENGINE_OBJS): CFLAGS += $(PROTO_CFLAGS)
 $(LIBFLUXENGINE_OBJS): CFLAGS += $(FATFS_CFLAGS)
 $(LIBFLUXENGINE_OBJS): CFLAGS += $(ADFLIB_CFLAGS)
+$(LIBFLUXENGINE_OBJS): CFLAGS += $(HFSUTILS_CFLAGS)
 
 $(call use-pkgconfig, $(LIBFLUXENGINE_LIB), $(LIBFLUXENGINE_OBJS), fmt)
---
+++ lib/vfs/machfs.cc
@@ -0,0 +1,6 @@
+#include "lib/globals.h"
+#include "lib/vfs/vfs.h"
+#include "lib/config.pb.h"
+#include <fmt/format.h>
+#include "libhfs.h"
+
