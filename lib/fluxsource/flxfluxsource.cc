5136dda59890900597a0e4ff8f4ca12be34ac859
dg <dg>
2022-11-29 20:17:24 +0000

    Made the FLX reader work.

--- lib/fluxsource/flxfluxsource.cc
+++ lib/fluxsource/flxfluxsource.cc
@@ -1,8 +1,8 @@
-#include "globals.h"
-#include "fluxmap.h"
+#include "lib/globals.h"
+#include "lib/fluxmap.h"
 #include "lib/fluxsource/fluxsource.pb.h"
-#include "fluxsource/fluxsource.h"
-#include <fstream>
+#include "lib/fluxsource/fluxsource.h"
+#include "lib/fluxsource/flx.h"
 
 class FlxFluxSource : public TrivialFluxSource
 {
@@ -14,33 +14,12 @@ public:
 public:
     std::unique_ptr<const Fluxmap> readSingleFlux(int track, int side) override
     {
-        std::string path = fmt::format("{}/@TR{:02}S{}.FLX", _path, track, side + 1);
-
-		std::ifstream is(path);
-        if (!is.good())
-            Error() << fmt::format("cannot access path '{}'", _path);
-
-        return convertStream(is);
+        std::string path =
+            fmt::format("{}/@TR{:02}S{}@.FLX", _path, track, side + 1);
+        return readFlxBytes(Bytes::readFromFile(path));
     }
 
     void recalibrate() {}
-
-private:
-	std::unique_ptr<const Fluxmap> convertStream(std::ifstream& is)
-	{
-		/* Skip header. */
-
-		for (;;)
-		{
-			int c = is.get();
-			if (!c)
-				break;
-			if (is.eof())
-				Error() << fmt::format("malformed FLX stream");
-		}
-
-		return nullptr;
-	}
 
 private:
     const std::string _path;
---
+++ tests/flx.cc
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "globals.h"
+#include "lib/fluxmap.h"
+#include "lib/fluxsource/flx.h"
+
+static void test_convert(const Bytes& flxbytes, const Bytes& fluxmapbytes)
+{
+    std::unique_ptr<Fluxmap> fluxmap = readFlxBytes(flxbytes);
+
+    if (fluxmap->rawBytes() != fluxmapbytes)
+    {
+        std::cout << "FLX bytes:" << std::endl;
+        hexdump(std::cout, flxbytes);
+        std::cout << std::endl << "Produced this:" << std::endl;
+        hexdump(std::cout, fluxmap->rawBytes());
+        std::cout << std::endl << "Expected this:" << std::endl;
+        hexdump(std::cout, fluxmapbytes);
+        abort();
+    }
+}
+
+static void test_stream_reader()
+{
+    test_convert(
+        Bytes{0},
+        Bytes{}
+    );
+
+    /* Simple one-byte intervals */
+    test_convert(
+        Bytes{ 0, 0x64, FLX_STOP },
+        Bytes{ 0xbc }
+    );
+
+    /* Index pulse */
+    test_convert(
+        Bytes{ 0, 0x64, FLX_INDEX, 0x64, FLX_STOP },
+        Bytes{ 0xfc, 0xbc }
+    );
+
+}
+
+int main(int argc, const char* argv[])
+{
+	test_stream_reader();
+    return 0;
+}
+
