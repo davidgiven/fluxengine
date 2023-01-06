3f85c9f006a9aad6822ea10e8000e9cd900a8c07
dg <dg>
2023-01-06 21:28:51 +0000

    Adjust timings to be more correct.

--- arch/agat/encoder.cc
+++ arch/agat/encoder.cc
@@ -32,38 +32,38 @@ private:
         }
     }
 
-	void writeBytes(const Bytes& bytes)
-	{
-		encodeMfm(_bits, _cursor, bytes, _lastBit);
-	}
+    void writeBytes(const Bytes& bytes)
+    {
+        encodeMfm(_bits, _cursor, bytes, _lastBit);
+    }
 
-	void writeByte(uint8_t byte)
-	{
-		Bytes b;
-		b.writer().write_8(byte);
-		writeBytes(b);
-	}
+    void writeByte(uint8_t byte)
+    {
+        Bytes b;
+        b.writer().write_8(byte);
+        writeBytes(b);
+    }
 
-	void writeFillerRawBytes(int count, uint16_t byte)
-	{
-		for (int i = 0; i < count; i++)
-			writeRawBits(byte, 16);
-	};
+    void writeFillerRawBytes(int count, uint16_t byte)
+    {
+        for (int i = 0; i < count; i++)
+            writeRawBits(byte, 16);
+    };
 
-	void writeFillerBytes(int count, uint8_t byte)
-	{
-		Bytes b{byte};
-		for (int i = 0; i < count; i++)
-			writeBytes(b);
-	};
+    void writeFillerBytes(int count, uint8_t byte)
+    {
+        Bytes b{byte};
+        for (int i = 0; i < count; i++)
+            writeBytes(b);
+    };
 
 public:
     std::unique_ptr<Fluxmap> encode(std::shared_ptr<const TrackInfo>& trackInfo,
         const std::vector<std::shared_ptr<const Sector>>& sectors,
         const Image& image) override
     {
-        auto trackLayout =
-            Layout::getLayoutOfTrack(trackInfo->logicalTrack, trackInfo->logicalSide);
+        auto trackLayout = Layout::getLayoutOfTrack(
+            trackInfo->logicalTrack, trackInfo->logicalSide);
 
         double clockRateUs = _config.target_clock_period_us() / 2.0;
         int bitsPerRevolution =
@@ -71,28 +71,28 @@ public:
         _bits.resize(bitsPerRevolution);
         _cursor = 0;
 
-		writeFillerRawBytes(_config.post_index_gap_bytes(), 0xaaaa);
+        writeFillerRawBytes(_config.post_index_gap_bytes(), 0xaaaa);
 
         for (const auto& sector : sectors)
-		{
-			/* Header */
+        {
+            /* Header */
 
-			writeFillerRawBytes(_config.pre_sector_gap_bytes(), 0xaaaa);
-			writeRawBits(SECTOR_ID, 64);
-			writeByte(0x5a);
-			writeByte((sector->logicalTrack << 1) | sector->logicalSide);
-			writeByte(sector->logicalSector);
-			writeByte(0x5a);
+            writeFillerRawBytes(_config.pre_sector_gap_bytes(), 0xaaaa);
+            writeRawBits(SECTOR_ID, 64);
+            writeByte(0x5a);
+            writeByte((sector->logicalTrack << 1) | sector->logicalSide);
+            writeByte(sector->logicalSector);
+            writeByte(0x5a);
 
-			/* Data */
+            /* Data */
 
-			writeFillerRawBytes(_config.pre_data_gap_bytes(), 0xaaaa);
-			auto data = sector->data.slice(0, AGAT_SECTOR_SIZE);
-			writeRawBits(DATA_ID, 64);
-			writeBytes(data);
-			writeByte(agatChecksum(data));
-			writeByte(0x5a);
-		}
+            writeFillerRawBytes(_config.pre_data_gap_bytes(), 0xaaaa);
+            auto data = sector->data.slice(0, AGAT_SECTOR_SIZE);
+            writeRawBits(DATA_ID, 64);
+            writeBytes(data);
+            writeByte(agatChecksum(data));
+            writeByte(0x5a);
+        }
 
         if (_cursor >= _bits.size())
             Error() << "track data overrun";
@@ -100,21 +100,19 @@ public:
 
         auto fluxmap = std::make_unique<Fluxmap>();
         fluxmap->appendBits(_bits,
-            calculatePhysicalClockPeriod(
-                _config.target_clock_period_us() * 1e3, _config.target_rotational_period_ms() * 1e6));
+            calculatePhysicalClockPeriod(_config.target_clock_period_us() * 1e3,
+                _config.target_rotational_period_ms() * 1e6));
         return fluxmap;
     }
 
 private:
     const AgatEncoderProto& _config;
-	uint32_t _cursor;
-	bool _lastBit;
+    uint32_t _cursor;
+    bool _lastBit;
     std::vector<bool> _bits;
 };
-
 
 std::unique_ptr<Encoder> createAgatEncoder(const EncoderProto& config)
 {
     return std::unique_ptr<Encoder>(new AgatEncoder(config));
 }
-
