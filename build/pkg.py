0dd99efad383305629f8dbe33c92af293311f58a
David Given <dg@cowlark.com>
2023-12-12 23:17:06 +0100

    Update ab.

--- build/pkg.py
+++ build/pkg.py
@@ -7,8 +7,6 @@ emit(
     """
 PKG_CONFIG ?= pkg-config
 PACKAGES := $(shell $(PKG_CONFIG) --list-all | cut -d' ' -f1 | sort)
-PCFILES := $(wildcard $(patsubst %,%/*.pc,$(subst :, ,$(shell $(PKG_CONFIG) --variable pc_path pkg-config))))
-$(OBJ)/build.mk: $(PCFILES)
 """
 )
 
