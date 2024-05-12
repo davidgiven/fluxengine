78d5584e2163e664442a46f19214d9f40043e1fe
David Given <dg@cowlark.com>
2024-05-13 00:32:57 +0200

    Add creeate, put and delete support to the CP/M filesystem driver.

--- tests/cpmfs.cc
+++ tests/cpmfs.cc
@@ -37,14 +37,22 @@ namespace
     };
 }
 
+static std::ostream& operator<<(std::ostream& stream, const Bytes& bytes)
+{
+    stream << '\n';
+    hexdump(stream, bytes);
+    return stream;
+}
+
 static Bytes createDirent(const std::string& filename,
     int extent,
     int records,
-    const std::initializer_list<int> blocks)
+    const std::initializer_list<int> blocks,
+    int user = 0)
 {
     Bytes dirent;
     ByteWriter bw(dirent);
-    bw.write_8(0);
+    bw.write_8(user);
     bw.append(filename);
     while (bw.pos != 12)
         bw.write_8(' ');
@@ -67,6 +75,21 @@ static void setBlock(
 {
     for (int i = 0; i < 8; i++)
         sectors->put(block, 0, i)->data = data.slice(i * 256, 256);
+}
+
+static Bytes getBlock(
+    const std::shared_ptr<SectorInterface>& sectors, int block, int length)
+{
+    Bytes bytes;
+    ByteWriter bw(bytes);
+
+    for (int i = 0; i < (length + 127) / 128; i++)
+    {
+        auto sector = sectors->get(block, 0, i);
+        bw.append(sector->data);
+    }
+
+    return bytes;
 }
 
 static void testPartialExtent()
@@ -113,6 +136,93 @@ static void testLogicalExtents()
     AssertThat(data[0x4000 * 2], Equals(3));
 }
 
+static void testBitmap()
+{
+    auto sectors = std::make_shared<TestSectorInterface>();
+    auto fs = Filesystem::createCpmFsFilesystem(
+        globalConfig()->filesystem(), sectors);
+
+    setBlock(sectors,
+        0,
+        createDirent("FILE", 1, 128, {1, 0, 0, 0, 0, 0, 0, 0, 2}) +
+            createDirent("FILE", 2, 128, {4}) + (blank_dirent * 62));
+
+    dynamic_cast<HasMount*>(fs.get())->mount();
+    std::vector<bool> bitmap =
+        dynamic_cast<HasBitmap*>(fs.get())->getBitmapForDebugging();
+    AssertThat(bitmap,
+        Equals(std::vector<bool>{
+            1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}));
+}
+
+static void testPutGet()
+{
+    auto sectors = std::make_shared<TestSectorInterface>();
+    auto fs = Filesystem::createCpmFsFilesystem(
+        globalConfig()->filesystem(), sectors);
+    fs->create(true, "volume");
+
+    fs->putFile(Path("0:FILE1"), Bytes{1, 2, 3, 4});
+    fs->putFile(Path("0:FILE2"), Bytes{5, 6, 7, 8});
+
+    dynamic_cast<HasMount*>(fs.get())->mount();
+    std::vector<bool> bitmap =
+        dynamic_cast<HasBitmap*>(fs.get())->getBitmapForDebugging();
+    AssertThat(bitmap,
+        Equals(std::vector<bool>{
+            1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}));
+
+    auto directory = getBlock(sectors, 0, 256).slice(0, 64);
+    AssertThat(directory,
+        Equals(createDirent("FILE1", 0, 1, {1}) +
+               createDirent("FILE2", 0, 1, {2})));
+
+    auto file1 = getBlock(sectors, 1, 8).slice(0, 8);
+    AssertThat(file1, Equals(Bytes{1, 2, 3, 4, 0, 0, 0, 0}));
+
+    auto file2 = getBlock(sectors, 2, 8).slice(0, 8);
+    AssertThat(file2, Equals(Bytes{5, 6, 7, 8, 0, 0, 0, 0}));
+}
+
+static void testPutBigFile()
+{
+    auto sectors = std::make_shared<TestSectorInterface>();
+    auto fs = Filesystem::createCpmFsFilesystem(
+        globalConfig()->filesystem(), sectors);
+    fs->create(true, "volume");
+
+    Bytes filedata;
+    ByteWriter bw(filedata);
+    while (filedata.size() < 0x9000)
+        bw.write_le32(bw.pos);
+
+    fs->putFile(Path("0:BIGFILE"), filedata);
+
+    auto directory = getBlock(sectors, 0, 256).slice(0, 64);
+    AssertThat(directory,
+        Equals(createDirent("BIGFILE",
+                   0,
+                   0x80,
+                   {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}) +
+               createDirent("BIGFILE", 2, 0x20, {17, 18})));
+}
+
+static auto testDelete()
+{
+    auto sectors = std::make_shared<TestSectorInterface>();
+    auto fs = Filesystem::createCpmFsFilesystem(
+        globalConfig()->filesystem(), sectors);
+    fs->create(true, "volume");
+
+    fs->putFile(Path("0:FILE1"), Bytes{1, 2, 3, 4});
+    fs->putFile(Path("0:FILE2"), Bytes{5, 6, 7, 8});
+    fs->deleteFile(Path("0:FILE1"));
+
+    auto directory = getBlock(sectors, 0, 256).slice(0, 64);
+    AssertThat(directory,
+        Equals((Bytes{0xe5} * 32) + createDirent("FILE2", 0, 1, {2})));
+}
+
 int main(void)
 {
     try
@@ -124,7 +234,7 @@ int main(void)
 
 			layout {
 				format_type: FORMATTYPE_80TRACK
-				tracks: 10
+				tracks: 20
 				sides: 1
 				layoutdata {
 					sector_size: 256
@@ -148,6 +258,10 @@ int main(void)
 
         testPartialExtent();
         testLogicalExtents();
+        testBitmap();
+        testPutGet();
+        testPutBigFile();
+        testDelete();
     }
     catch (const ErrorException& e)
     {
