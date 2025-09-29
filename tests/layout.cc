6979567429c9d9c354ee8af576a71f9343ee4172
David Given <dg@cowlark.com>
2025-09-29 20:54:05 +0200

    Add basic tests for DiskLayout.

--- tests/layout.cc
+++ tests/layout.cc
@@ -42,15 +42,29 @@ static void test_physical_sectors()
 		}
 	)M");
 
-    auto layout = Layout::getLayoutOfTrack(0, 0);
-    AssertThat(
-        layout->naturalSectorOrder, Equals(std::vector<unsigned>{0, 1, 2, 3}));
-    AssertThat(
-        layout->diskSectorOrder, Equals(std::vector<unsigned>{0, 2, 1, 3}));
-    AssertThat(layout->filesystemSectorOrder,
-        Equals(std::vector<unsigned>{0, 1, 2, 3}));
+    {
+        auto layout = Layout::getLayoutOfTrack(0, 0);
+        AssertThat(layout->naturalSectorOrder,
+            Equals(std::vector<unsigned>{0, 1, 2, 3}));
+        AssertThat(
+            layout->diskSectorOrder, Equals(std::vector<unsigned>{0, 2, 1, 3}));
+        AssertThat(layout->filesystemSectorOrder,
+            Equals(std::vector<unsigned>{0, 1, 2, 3}));
+    }
 
-	auto diskLayout = createDiskLayout();
+    {
+        auto diskLayout = createDiskLayout();
+        auto physicalLayout = diskLayout->layoutByPhysicalLocation.at({0, 0});
+        auto layout = physicalLayout->logicalTrackLayout;
+        AssertThat(
+            diskLayout->layoutByLogicalLocation.at({0, 0}), Equals(layout));
+        AssertThat(layout->naturalSectorOrder,
+            Equals(std::vector<unsigned>{0, 1, 2, 3}));
+        AssertThat(
+            layout->diskSectorOrder, Equals(std::vector<unsigned>{0, 2, 1, 3}));
+        AssertThat(layout->filesystemSectorOrder,
+            Equals(std::vector<unsigned>{0, 1, 2, 3}));
+    }
 }
 
 static void test_logical_sectors()
@@ -83,13 +97,29 @@ static void test_logical_sectors()
 		}
 	)M");
 
-    auto layout = Layout::getLayoutOfTrack(0, 0);
-    AssertThat(
-        layout->naturalSectorOrder, Equals(std::vector<unsigned>{0, 1, 2, 3}));
-    AssertThat(
-        layout->diskSectorOrder, Equals(std::vector<unsigned>{0, 1, 2, 3}));
-    AssertThat(layout->filesystemSectorOrder,
-        Equals(std::vector<unsigned>{0, 2, 1, 3}));
+    {
+        auto layout = Layout::getLayoutOfTrack(0, 0);
+        AssertThat(layout->naturalSectorOrder,
+            Equals(std::vector<unsigned>{0, 1, 2, 3}));
+        AssertThat(
+            layout->diskSectorOrder, Equals(std::vector<unsigned>{0, 1, 2, 3}));
+        AssertThat(layout->filesystemSectorOrder,
+            Equals(std::vector<unsigned>{0, 2, 1, 3}));
+    }
+
+    {
+        auto diskLayout = createDiskLayout();
+        auto physicalLayout = diskLayout->layoutByPhysicalLocation.at({0, 0});
+        auto layout = physicalLayout->logicalTrackLayout;
+        AssertThat(
+            diskLayout->layoutByLogicalLocation.at({0, 0}), Equals(layout));
+        AssertThat(layout->naturalSectorOrder,
+            Equals(std::vector<unsigned>{0, 1, 2, 3}));
+        AssertThat(
+            layout->diskSectorOrder, Equals(std::vector<unsigned>{0, 1, 2, 3}));
+        AssertThat(layout->filesystemSectorOrder,
+            Equals(std::vector<unsigned>{0, 2, 1, 3}));
+    }
 }
 
 static void test_both_sectors()
@@ -122,13 +152,29 @@ static void test_both_sectors()
 		}
 	)M");
 
-    auto layout = Layout::getLayoutOfTrack(0, 0);
-    AssertThat(
-        layout->naturalSectorOrder, Equals(std::vector<unsigned>{0, 1, 2, 3}));
-    AssertThat(
-        layout->diskSectorOrder, Equals(std::vector<unsigned>{3, 2, 1, 0}));
-    AssertThat(layout->filesystemSectorOrder,
-        Equals(std::vector<unsigned>{0, 2, 1, 3}));
+    {
+        auto layout = Layout::getLayoutOfTrack(0, 0);
+        AssertThat(layout->naturalSectorOrder,
+            Equals(std::vector<unsigned>{0, 1, 2, 3}));
+        AssertThat(
+            layout->diskSectorOrder, Equals(std::vector<unsigned>{3, 2, 1, 0}));
+        AssertThat(layout->filesystemSectorOrder,
+            Equals(std::vector<unsigned>{0, 2, 1, 3}));
+    }
+
+    {
+        auto diskLayout = createDiskLayout();
+        auto physicalLayout = diskLayout->layoutByPhysicalLocation.at({0, 0});
+        auto layout = physicalLayout->logicalTrackLayout;
+        AssertThat(
+            diskLayout->layoutByLogicalLocation.at({0, 0}), Equals(layout));
+        AssertThat(layout->naturalSectorOrder,
+            Equals(std::vector<unsigned>{0, 1, 2, 3}));
+        AssertThat(
+            layout->diskSectorOrder, Equals(std::vector<unsigned>{3, 2, 1, 0}));
+        AssertThat(layout->filesystemSectorOrder,
+            Equals(std::vector<unsigned>{0, 2, 1, 3}));
+    }
 }
 
 static void test_skew()
@@ -154,11 +200,27 @@ static void test_skew()
 		}
 	)M");
 
-    auto layout = Layout::getLayoutOfTrack(0, 0);
-    AssertThat(layout->naturalSectorOrder,
-        Equals(std::vector<unsigned>{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}));
-    AssertThat(layout->diskSectorOrder,
-        Equals(std::vector<unsigned>{0, 6, 1, 7, 2, 8, 3, 9, 4, 10, 5, 11}));
+    {
+        auto layout = Layout::getLayoutOfTrack(0, 0);
+        AssertThat(layout->naturalSectorOrder,
+            Equals(
+                std::vector<unsigned>{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}));
+        AssertThat(layout->diskSectorOrder,
+            Equals(
+                std::vector<unsigned>{0, 6, 1, 7, 2, 8, 3, 9, 4, 10, 5, 11}));
+    }
+
+    {
+        auto diskLayout = createDiskLayout();
+        auto physicalLayout = diskLayout->layoutByPhysicalLocation.at({0, 0});
+        auto layout = physicalLayout->logicalTrackLayout;
+        AssertThat(layout->naturalSectorOrder,
+            Equals(
+                std::vector<unsigned>{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}));
+        AssertThat(layout->diskSectorOrder,
+            Equals(
+                std::vector<unsigned>{0, 6, 1, 7, 2, 8, 3, 9, 4, 10, 5, 11}));
+    }
 }
 
 int main(int argc, const char* argv[])
