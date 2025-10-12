7341cec2c428c1bad985c318ed036177003c815c
David Given <dg@cowlark.com>
2025-10-13 00:30:24 +0200

    Add missing file.

---
+++ lib/data/decoded.cc
@@ -0,0 +1,54 @@
+#include "lib/core/globals.h"
+#include "lib/data/decoded.h"
+#include "lib/data/image.h"
+#include "lib/data/layout.h"
+#include "lib/data/sector.h"
+#include "protocol.h"
+
+namespace
+{
+    struct pair_to_range_t
+    {
+        template <typename I>
+        friend constexpr auto operator|(
+            std::pair<I, I> const& pr, pair_to_range_t)
+        {
+            return std::ranges::subrange(pr.first, pr.second);
+        }
+    };
+    inline constexpr pair_to_range_t pair_to_range{};
+}
+
+void DecodedDisk::populateTrackDataFromImage(const DiskLayout& diskLayout)
+{
+    tracksByPhysicalLocation.clear();
+    sectorsByPhysicalLocation.clear();
+
+    std::multimap<CylinderHead, std::shared_ptr<const Sector>>
+        sectorsGroupedByTrack;
+    for (const auto& sector : *image)
+        sectorsGroupedByTrack.insert(
+            std::make_pair(sector->physicalLocation.value(), sector));
+
+    for (const auto& physicalLocation :
+        std::views::keys(sectorsGroupedByTrack) | std::ranges::to<std::set>())
+    {
+        const auto& ptl =
+            diskLayout.layoutByPhysicalLocation.at(physicalLocation);
+        const auto& ltl = ptl->logicalTrackLayout;
+
+        auto decodedTrack = std::make_shared<DecodedTrack>();
+        decodedTrack->ltl = ltl;
+        decodedTrack->ptl = ptl;
+        tracksByPhysicalLocation.insert(
+            std::make_pair(physicalLocation, decodedTrack));
+
+        for (auto& [ch, sector] :
+            sectorsGroupedByTrack.equal_range(physicalLocation) | pair_to_range)
+        {
+            decodedTrack->sectors.push_back(sector);
+            sectorsByPhysicalLocation.insert(
+                std::make_pair(physicalLocation, sector));
+        }
+    }
+}
