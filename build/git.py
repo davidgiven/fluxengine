a8a05b94316b2fc497370494133e6fc13ace61ce
David Given <dg@cowlark.com>
2026-01-05 22:26:50 +0000

    Add missing file.

---
+++ build/git.py
@@ -0,0 +1,19 @@
+from build.ab import Rule, simplerule
+from build.utils import add_wildcard_dependency
+
+
+@Rule
+def git_repository(self, name, url, branch, path):
+    simplerule(
+        replaces=self,
+        outs=[f"{path}/.git/config"],
+        commands=[
+            f"rmdir {path}/.git",
+            f"git clone -q {url} --depth=1 -c advice.detachedHead=false -b {branch} {path}",
+        ],
+        sandbox=False,
+        generator=True,
+        label="GITREPOSITORY",
+    )
+
+    add_wildcard_dependency(self, f"{path}/**/*", exclude="**/.git/**")
