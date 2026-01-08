87a407d9c8c4c75e607defabcf236315df9f192b
David Given <dg@cowlark.com>
2026-01-08 15:05:56 +0000

    Allow git repositories to be checked out at a particular commit, not just branch.

--- build/git.py
+++ build/git.py
@@ -3,14 +3,21 @@ from build.utils import add_wildcard_dependency
 
 
 @Rule
-def git_repository(self, name, url, branch, path):
+def git_repository(self, name, url, branch, path, commit=None):
     simplerule(
         replaces=self,
         outs=[f"{path}/.git/config"],
         commands=[
             f"rmdir {path}/.git",
             f"git clone -q {url} --depth=1 -c advice.detachedHead=false -b {branch} {path}",
-        ],
+        ]
+        + (
+            [
+                f"cd {path} && git fetch --depth=1 origin {commit} && git checkout {commit}"
+            ]
+            if commit
+            else []
+        ),
         sandbox=False,
         generator=True,
         label="GITREPOSITORY",
