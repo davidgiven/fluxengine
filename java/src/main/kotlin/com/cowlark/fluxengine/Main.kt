99f1764dc69c88f1846658577e2665d7393b2c80
David Given <dg@cowlark.com>
2026-08-04 17:22:00 +0200

    Remove the old Kotlin stuff.

--- java/src/main/kotlin/com/cowlark/fluxengine/Main.kt
+++
@@ -1,6 +0,0 @@
-// Main.kt deprecated - replaced by java/src/main/java/com/cowlark/fluxengine/Main.java
-// Remove this file when you are ready to fully delete Kotlin sources.
-
-package com.cowlark.fluxengine
-
-// Deprecated placeholder
