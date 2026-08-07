f753bbd0ea3bc2c851172f65b9cb5f3802f69ee9
David Given <dg@cowlark.com>
2026-08-08 00:58:18 +0200

    Port the Kryoflux libraries.

---
+++ javatests/com/cowlark/fluxengine/data/KryofluxTest.java
@@ -0,0 +1,73 @@
+package com.cowlark.fluxengine.data;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.cowlark.fluxengine.core.Bytes;
+import java.util.Arrays;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class KryofluxTest
+{
+    @Test
+    public void test_stream_reader()
+    {
+        testConvert(Bytes.of(), Bytes.of());
+
+        /* Simple one-byte intervals */
+        testConvert(Bytes.of(0x20, 0x20, 0x20, 0x20), Bytes.of(0x8f, 0x8f, 0x8f, 0x8f));
+
+        /* One-and-a-half-byte intervals */
+        testConvert(
+            Bytes.of(0x20, 0x00, 0x10, 0x20, 0x01, 0x10, 0x20),
+            Bytes.of(0x8f, 0x87, 0x8f, 0x3f, 0x3f, 0x89, 0x8f));
+
+        /* Two-byte intervals */
+        testConvert(
+            Bytes.of(0x20, 0x0c, 0x00, 0x10, 0x20, 0x0c, 0x01, 0x10, 0x20),
+            Bytes.of(0x8f, 0x87, 0x8f, 0x3f, 0x3f, 0x89, 0x8f));
+
+        /* Overflow */
+        testConvert(
+            Bytes.of(0x20, 0x0b, 0x10, 0x20),
+            Bytes.of(0x8f).concat(unsignedBytes(0x207)).concat(Bytes.of(0xa9, 0x8f)));
+
+        /* Single-byte nop */
+        testConvert(Bytes.of(0x20, 0x08, 0x20), Bytes.of(0x8f, 0x8f));
+
+        /* Double-byte nop */
+        testConvert(Bytes.of(0x20, 0x09, 0xde, 0x20), Bytes.of(0x8f, 0x8f));
+
+        /* Triple-byte nop */
+        testConvert(Bytes.of(0x20, 0x0a, 0xde, 0xad, 0x20), Bytes.of(0x8f, 0x8f));
+
+        /* OOB block */
+        testConvert(
+            Bytes.of(
+                0x20, /* data before */
+                0x0d, /* OOB */
+                0xaa, /* type byte */
+                0x01,
+                0x00, /* size of payload, little-endian */
+                0x55, /* payload */
+                0x20  /* data continues */
+            ),
+            Bytes.of(0x8f, 0x8f));
+    }
+
+    private static void testConvert(Bytes kyrofluxBytes, Bytes expectedFluxmapBytes)
+    {
+        Fluxmap fluxmap = Kryoflux.readStream(kyrofluxBytes);
+        assertThat(fluxmap.rawBytes().toByteArray())
+                .isEqualTo(expectedFluxmapBytes.toByteArray());
+    }
+
+    private static Bytes unsignedBytes(int count)
+    {
+        byte[] data = new byte[count];
+        Arrays.fill(data, (byte) 0x3f);
+        return new Bytes(data);
+    }
+}
