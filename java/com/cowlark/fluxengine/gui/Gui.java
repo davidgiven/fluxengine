4de94184e703e5787a3c2ff748e4c0d1b281ea8b
David Given <dg@cowlark.com>
2026-08-10 14:46:08 +0000

    Integrate flatlaf.

--- java/com/cowlark/fluxengine/gui/Gui.java
+++ java/com/cowlark/fluxengine/gui/Gui.java
@@ -1,8 +1,10 @@
 package com.cowlark.fluxengine.gui;
 
+import com.formdev.flatlaf.FlatLightLaf;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 import javax.swing.WindowConstants;
 
 /**
@@ -12,7 +14,19 @@ public class Gui
 {
     public static void main(String[] args)
     {
-        SwingUtilities.invokeLater(Gui::createAndShowGui);
+        SwingUtilities.invokeLater(() -> {
+            setupLookAndFeel();
+            createAndShowGui();
+        });
+    }
+
+    private static void setupLookAndFeel()
+    {
+        try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private static void createAndShowGui()
