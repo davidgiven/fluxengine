4e7568e5a0485503f11c567b40202db2b82bf33c
David Given <dg@cowlark.com>
2026-08-13 01:45:39 +0200

    Integrate SwingTree.

--- java/com/cowlark/fluxengine/gui/Gui.java
+++ java/com/cowlark/fluxengine/gui/Gui.java
@@ -1,11 +1,18 @@
 package com.cowlark.fluxengine.gui;
 
+import static swingtree.UI.label;
+import static swingtree.UI.menu;
+import static swingtree.UI.menuItem;
+import static swingtree.UI.of;
+import static swingtree.UI.panel;
+
 import com.formdev.flatlaf.FlatDarkLaf;
 import com.google.common.collect.ImmutableList;
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.UIManager;
-import javax.swing.WindowConstants;
+import swingtree.UI;
+import swingtree.threading.EventProcessor;
 
 /**
  * The FluxEngine GUI, ported from src/gui/main.cc.
@@ -16,17 +23,29 @@ public class Gui
     {
         UIManager.setLookAndFeel(new FlatDarkLaf());
         System.setProperty("apple.laf.useScreenMenuBar", "true");
-        SwingUtilities.invokeLater(() -> {
-            createAndShowGui();
-        });
-    }
 
-    private static void createAndShowGui()
-    {
-        JFrame frame = new NewJFrame();
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+        UI.MenuBar menuBar = of(new UI.MenuBar())
+                .add(menu("File")
+                        .add(menuItem("About FluxEngine...").onClick(it ->
+                                JOptionPane.showMessageDialog(
+                                        null,
+                                        "FluxEngine\nA disk-flux reader/writer",
+                                        "About FluxEngine",
+                                        JOptionPane.INFORMATION_MESSAGE)))
+                        .add(menuItem("Exit").onClick(it -> System.exit(0))))
+                .get(UI.MenuBar.class);
+
+        UI.show("FluxEngine", frame -> {
+            frame.setJMenuBar(menuBar);
+            frame.setSize(800, 600);
+            frame.setLocationRelativeTo(null);
+
+            return panel("fill")
+                    .add(label("FluxEngine"))
+                    .get(JPanel.class);
+        });
+
+        EventProcessor.DECOUPLED.join();
     }
 
     public static void main(String[] args)
