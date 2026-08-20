25129ae6617cd2058ccf1c7556c07fb6f7c7168f
David Given <dg@cowlark.com>
2026-08-21 00:35:48 +0200

    Don't use a split panel for the configuration pane.

--- java/com/cowlark/fluxengine/gui/ApplicationFrame.java
+++ java/com/cowlark/fluxengine/gui/ApplicationFrame.java
@@ -8,8 +8,6 @@ import static swingtree.UIFactoryMethods.splitPane;
 import static swingtree.UIFactoryMethods.tab;
 import static swingtree.UIFactoryMethods.tabbedPane;
 import static swingtree.UILayoutConstants.BOTTOM;
-import static swingtree.UILayoutConstants.LEFT;
-import static swingtree.UILayoutConstants.RIGHT;
 import static swingtree.UILayoutConstants.TOP;
 
 import swingtree.UI;
@@ -44,36 +42,29 @@ public class ApplicationFrame extends JFrame
                     frame.setSize(1280, 720);
                     frame.setLocationRelativeTo(null);
                 })
-                .add(panel("fill, wrap 1").add("grow, push",
-                                splitPane(UI.Align.HORIZONTAL).add(LEFT,
-                                                tabbedPane().add(tab("Configuration").add(scrollPane().add(of(
-                                                        configurationPanel)))))
-                                        .add(RIGHT,
-                                                splitPane(UI.Align.VERTICAL).peek(pane -> pane.setResizeWeight(
-                                                                1.0))
-                                                        .add(TOP,
-                                                                tabbedPane().add(tab("Visualiser").add(of(
-                                                                                visualiserPanel)))
-                                                                        .add(tab("Image").add(of(imagePanel)))
-                                                                        .add(tab("Log").add(of(logPanel))))
-                                                        .add(BOTTOM,
-                                                                tabbedPane().add(tab("Summary").add(panel(
-                                                                        "fillx, wrap 1, aligny " +
-                                                                                "center").add(
-                                                                                "growx, h 100pt!",
-                                                                                of(summaryPanel))
-                                                                        .add("growx",
-                                                                                panel("wrap 3, " +
-                                                                                        "alignx " +
-                                                                                        "center").add(button(
-                                                                                                "Read disk").onClick(
-                                                                                                model::onReadDisk))
-                                                                                        .add(button(
-                                                                                                "Reread disk").onClick(
-                                                                                                model::onRereadDisk))
-                                                                                        .add(button(
-                                                                                                "Write disk").onClick(
-                                                                                                model::onWriteDisk))))))))
-                        .add("growx", statusbarPanel));
+                .add(panel("fill, wrap 2").add("growy",
+                                tabbedPane().add(tab("Configuration").add(scrollPane().add(of(
+                                        configurationPanel)))))
+                        .add("grow, push",
+                                splitPane(UI.Align.VERTICAL).peek(pane -> pane.setResizeWeight(1.0))
+                                        .add(TOP,
+                                                tabbedPane().add(tab("Visualiser").add(of(
+                                                                visualiserPanel)))
+                                                        .add(tab("Image").add(of(imagePanel)))
+                                                        .add(tab("Log").add(of(logPanel))))
+                                        .add(BOTTOM,
+                                                tabbedPane().add(tab("Summary").add(panel(
+                                                        "fillx, wrap 1, aligny " + "center").add(
+                                                                "growx, h 100pt!",
+                                                                of(summaryPanel))
+                                                        .add("growx",
+                                                                panel("wrap 3, " + "alignx " +
+                                                                        "center").add(button(
+                                                                                "Read disk").onClick(model::onReadDisk))
+                                                                        .add(button("Reread disk").onClick(
+                                                                                model::onRereadDisk))
+                                                                        .add(button("Write disk").onClick(
+                                                                                model::onWriteDisk)))))))
+                        .add("growx, span 2", statusbarPanel));
     }
 }
