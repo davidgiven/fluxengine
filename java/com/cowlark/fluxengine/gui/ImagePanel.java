98f3ff8369eaa5168d624803b952b592b564d721
David Given <dg@cowlark.com>
2026-08-22 00:42:37 +0200

    Image panel tweaks.

--- java/com/cowlark/fluxengine/gui/ImagePanel.java
+++ java/com/cowlark/fluxengine/gui/ImagePanel.java
@@ -3,6 +3,7 @@ package com.cowlark.fluxengine.gui;
 import com.cowlark.fluxengine.data.Disk;
 import com.formdev.flatlaf.util.UIScale;
 import org.exbin.auxiliary.binary_data.EmptyBinaryData;
+import org.exbin.bined.CodeCharactersCase;
 import org.exbin.bined.swing.basic.CodeArea;
 import sprouts.From;
 import sprouts.Viewable;
@@ -11,6 +12,7 @@ import javax.swing.UIManager;
 import javax.swing.border.LineBorder;
 import java.awt.BorderLayout;
 import java.awt.Font;
+import java.nio.charset.StandardCharsets;
 
 /**
  * A panel which displays the raw bytes of the loaded disk image in a hex
@@ -32,6 +34,8 @@ public class ImagePanel extends JPanel
         codeArea.setBorder(new LineBorder(UIManager.getColor("TextArea.background"), 3));
         codeArea.setCodeFont(font);
         codeArea.setContentData(new EmptyBinaryData());
+        codeArea.setCharset(StandardCharsets.ISO_8859_1);
+        codeArea.setCodeCharactersCase(CodeCharactersCase.LOWER);
         add(codeArea, BorderLayout.CENTER);
 
         Viewable.cast(model.getDisk()).onChange(From.VIEW_MODEL, it -> updateContent());
